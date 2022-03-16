607d98dd3594265b1aec8e534ccec40e41757459
Alex Keating <keating.dev@protonmail.com>
2022-03-15 23:44:15 -0400

    Change prot var name

--- apps/airtable-api/src/main.ts
+++ apps/airtable-api/src/main.ts
@@ -124,7 +124,7 @@ app.post(
   })
 );
 
-const port = process.env.port || 3333;
+const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
