1ba243bfe8fa6db16b4110cf3d51824e4c20791d
Alex Keating <keating.dev@protonmail.com>
2022-04-07 21:42:49 -0400

    Initial prisma

---
+++ apps/protocol-api/src/main.ts
@@ -0,0 +1,25 @@
+console.log('Hello World!');
+
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+// A `main` function so that you can use async/await
+
+async function main() {
+  const allUsers = await prisma.user.findMany({
+    include: { posts: true },
+  });
+
+  // use `console.dir` to print nested objects
+  console.dir(allUsers, { depth: null });
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
