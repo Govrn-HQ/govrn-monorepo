d3de61b068259efcd8f4d0ed0332e57b9b3b4d65
Alex Keating <keating.dev@protonmail.com>
2022-04-14 10:50:27 -0400

    Working api calls

--- apps/protocol-api/src/main.ts
+++ apps/protocol-api/src/main.ts
@@ -1,9 +1,13 @@
 import 'reflect-metadata';
 import { buildSchemaSync } from 'type-graphql';
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
 import { resolvers } from './prisma/generated/type-graphql';
 
 import { graphqlHTTP } from 'express-graphql';
+
+const prisma = new PrismaClient();
 
 const schema = buildSchemaSync({
   resolvers,
@@ -14,6 +18,8 @@ app.use(
   '/graphql',
   graphqlHTTP({
     schema,
+    graphiql: true,
+    context: { prisma },
   })
 );
 app.listen(4000);
