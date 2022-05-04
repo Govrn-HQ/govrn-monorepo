9d7e2e0d18e223e4aa7723c8523b9a00cf2ad302
jonathanprozzi <jonathanprozzi@gmail.com>
2022-05-04 11:10:18 -0400

    Scaffolding for the protocol-frontend package

---
+++ apps/protocol-frontend/src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom';
+
+import R from './Routes';
+
+ReactDOM.render(
+  <StrictMode>
+    <R />
+  </StrictMode>,
+  document.getElementById('root')
+);
---
+++ apps/protocol-frontend/src/pages/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const App = () => {
+  return (
+    <Container>
+      <h1>App</h1>
+    </Container>
+  );
+};
+
+export default App;
