d18d20f011abd0b8f2b7edd313acd59e4132db12
Alex Keating <keating.dev@protonmail.com>
2022-05-06 15:31:47 -0400

    Fix build and serve

--- apps/protocol-frontend/vite.config.ts
+++ apps/protocol-frontend/vite.config.ts
@@ -1,30 +1,13 @@
 import { defineConfig } from 'vite';
-import strip from '@rollup/plugin-strip';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { resolve } from 'path';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [strip(), react(), tsconfigPaths()],
-  define: {
-    'process.env': process.env,
-  },
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
-      stream: 'stream-browserify',
-      url: 'url-polyfill',
-      http: 'http-browserify',
-      https: 'http-browserify',
-      // process: 'process-es6',
-      '~~': resolve(__dirname, 'src'),
-    },
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
+      process: 'process-es6',
     },
   },
 });
