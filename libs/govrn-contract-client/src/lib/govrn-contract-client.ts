111bb0c8f9d7ecfa399457f9dcc15854a9b2a8a1
Alex Keating <keating.dev@protonmail.com>
2022-05-10 14:04:35 -0400

    WIP: Contract

--- libs/govrn-contract-client/src/lib/govrn-contract-client.ts
+++ libs/govrn-contract-client/src/lib/govrn-contract-client.ts
@@ -1,3 +1,19 @@
-export function govrnContractClient(): string {
-  return 'govrn-contract-client';
+import { Govrn__factory } from './generated'
+
+// attest
+// mint
+// typechain --target ethers-v5 --out-dir app/contracts './node_modules/neufund-contracts/build/contracts/*.json'
+
+const networks = {
+	31337: {
+		"address": "0x700b6a60ce7eaaea56f065753d8dcb9653dbad35"
+
+	}
+}
+
+export class GovrnContract {
+  constructor(chainId: number) {
+
+  }
+}
 }
