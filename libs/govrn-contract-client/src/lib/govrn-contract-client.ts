15ea16619a5cd73c4645296f1e13d72e1c534b87
Alex Keating <keating.dev@protonmail.com>
2022-05-10 16:14:22 -0400

    Initial contract functions

--- libs/govrn-contract-client/src/lib/govrn-contract-client.ts
+++ libs/govrn-contract-client/src/lib/govrn-contract-client.ts
@@ -1,19 +1,59 @@
-import { Govrn__factory } from './generated'
+import { ethers } from 'ethers';
+import { Govrn__factory, Govrn } from './generated';
 
 // attest
 // mint
 // typechain --target ethers-v5 --out-dir app/contracts './node_modules/neufund-contracts/build/contracts/*.json'
 
-const networks = {
-	31337: {
-		"address": "0x700b6a60ce7eaaea56f065753d8dcb9653dbad35"
+type MintArgs = {
+  name: ethers.BytesLike;
+  details: ethers.BytesLike;
+  dateOfSubmission: ethers.BigNumberish;
+  dateOfEngagement: ethers.BigNumberish;
+  proof: ethers.BytesLike;
+  partners: string[];
+  overrides?: ethers.Overrides & { from?: string | Promise<string> };
+};
 
-	}
-}
+type AttestArgs = {
+  contribution: ethers.BigNumberish;
+  confidence: ethers.BigNumberish;
+  overrides?: ethers.Overrides & { from?: string | Promise<string> };
+};
+
+const networks: { [key: number]: { address: string } } = {
+  31337: {
+    address: process.env['LOCAL_CONTRACT'] || '',
+  },
+};
 
 export class GovrnContract {
-  constructor(chainId: number) {
-
+  govrn: Govrn;
+  constructor(chainId: number, provider: ethers.providers.Provider) {
+    const networkConfig = networks[chainId];
+    if (!networkConfig) {
+      throw Error(`No network config for chainId ${chainId}`);
+    }
+    this.govrn = Govrn__factory.connect(networkConfig.address, provider);
   }
-}
+
+  public async mint(args: MintArgs) {
+    return await this.govrn.mint(
+      args.name,
+      args.details,
+      args.dateOfSubmission,
+      args.dateOfEngagement,
+      args.proof,
+      args.partners,
+      args.overrides
+    );
+  }
+
+  public async attest(args: AttestArgs) {
+    return await this.govrn.attest(
+      args.contribution,
+      args.confidence,
+      args.overrides
+    );
+  }
 }
