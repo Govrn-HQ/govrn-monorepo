56d4093cb9ff655c8ab617047c0def488c65a234
jonathanprozzi <jonathanprozzi@gmail.com>
2022-05-11 23:37:00 -0400

    abstracted the Empty states into an EmptyContributions component -- extra explanation copy is added if user is on /attestations

--- apps/protocol-frontend/src/components/AttestationsTableShell.tsx
+++ apps/protocol-frontend/src/components/AttestationsTableShell.tsx
@@ -24,6 +24,8 @@ import { HiOutlineChevronDown, HiOutlineChevronUp } from 'react-icons/hi';
 import { useTable, useSortBy } from 'react-table';
 import PageHeading from './PageHeading';
 import AttestationsTable from './AttestationsTable';
+import EmptyContributions from './EmptyContributions';
+
 import { mockAttestations } from '../utils/mockData';
 
 const AttestationsTableShell = () => {
@@ -36,61 +38,65 @@ const AttestationsTableShell = () => {
       maxWidth="1200px"
     >
       <PageHeading>Attestations</PageHeading>
-      <Box
-        background="white"
-        boxShadow="sm"
-        borderRadius={useBreakpointValue({ base: 'none', md: 'lg' })}
-      >
-        <Stack spacing="5">
-          <Box paddingX={{ base: '4', md: '6' }} paddingTop="5">
-            <Stack
-              direction={{ base: 'column', md: 'row' }}
-              justify="space-between"
-            >
-              <Text fontSize="lg" fontWeight="medium">
-                DAO Contributions
-              </Text>
-              <Button
-                bgColor="brand.primary.50"
-                color="brand.primary.600"
-                transition="all 100ms ease-in-out"
-                _hover={{ bgColor: 'brand.primary.100' }}
-                flexBasis="10%"
-                colorScheme="brand.primary"
+      {mockAttestations && mockAttestations.length < 0 ? (
+        <Box
+          background="white"
+          boxShadow="sm"
+          borderRadius={useBreakpointValue({ base: 'none', md: 'lg' })}
+        >
+          <Stack spacing="5">
+            <Box paddingX={{ base: '4', md: '6' }} paddingTop="5">
+              <Stack
+                direction={{ base: 'column', md: 'row' }}
+                justify="space-between"
               >
-                Mint
-              </Button>
-              {/* <InputGroup maxW="xs">
+                <Text fontSize="lg" fontWeight="medium">
+                  DAO Contributions
+                </Text>
+                <Button
+                  bgColor="brand.primary.50"
+                  color="brand.primary.600"
+                  transition="all 100ms ease-in-out"
+                  _hover={{ bgColor: 'brand.primary.100' }}
+                  flexBasis="10%"
+                  colorScheme="brand.primary"
+                >
+                  Mint
+                </Button>
+                {/* <InputGroup maxW="xs">
                 <InputLeftElement pointerEvents="none">
                   <Icon as={FiSearch} color="muted" boxSize="5" />
                 </InputLeftElement>
                 <Input placeholder="Search" />
               </InputGroup> */}
-            </Stack>
-          </Box>
-          <Box overflowX="auto">
-            <AttestationsTable attestationsData={mockAttestations} />
-          </Box>
-          <Box px={{ base: '4', md: '6' }} pb="5">
-            <HStack spacing="3" justify="space-between">
-              {!isMobile && (
-                <Text color="muted" fontSize="sm">
-                  Showing 1 to (x) of (y) results
-                </Text>
-              )}
-              <ButtonGroup
-                spacing="3"
-                justifyContent="space-between"
-                width={{ base: 'full', md: 'auto' }}
-                variant="secondary"
-              >
-                <Button>Previous</Button>
-                <Button>Next</Button>
-              </ButtonGroup>
-            </HStack>
-          </Box>
-        </Stack>
-      </Box>
+              </Stack>
+            </Box>
+            <Box overflowX="auto">
+              <AttestationsTable attestationsData={mockAttestations} />
+            </Box>
+            <Box px={{ base: '4', md: '6' }} pb="5">
+              <HStack spacing="3" justify="space-between">
+                {!isMobile && (
+                  <Text color="muted" fontSize="sm">
+                    Showing 1 to (x) of (y) results
+                  </Text>
+                )}
+                <ButtonGroup
+                  spacing="3"
+                  justifyContent="space-between"
+                  width={{ base: 'full', md: 'auto' }}
+                  variant="secondary"
+                >
+                  <Button>Previous</Button>
+                  <Button>Next</Button>
+                </ButtonGroup>
+              </HStack>
+            </Box>
+          </Stack>
+        </Box>
+      ) : (
+        <EmptyContributions />
+      )}
     </Container>
   );
 };
