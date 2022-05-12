9d1607fb097cd195b397afaa5f3c3e1ceb0ea4da
Alex Keating <keating.dev@protonmail.com>
2022-05-12 16:30:27 -0400

    Remove test

--- libs/govrn-contract-client/src/lib/govrn-contract-client.spec.ts
+++
@@ -1,7 +0,0 @@
-import { govrnContractClient } from './govrn-contract-client';
-
-describe('govrnContractClient', () => {
-  it('should work', () => {
-    expect(govrnContractClient()).toEqual('govrn-contract-client');
-  });
-});
