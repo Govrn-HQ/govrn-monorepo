dc80113c05409e54b75fce5aed2cb952ca5476e8
jonathanprozzi <jonathanprozzi@gmail.com>
2022-05-15 16:33:42 -0400

    added dropdown to the ConnectWallet with Sign Out and Copy address

--- apps/protocol-frontend/src/components/ConnectWallet.tsx
+++ apps/protocol-frontend/src/components/ConnectWallet.tsx
@@ -1,10 +1,22 @@
 import { formatAddress, useWallet } from '@raidguild/quiver';
-import { Button, Text } from '@chakra-ui/react';
-import { FiKey } from 'react-icons/fi';
+import {
+  Button,
+  Text,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Icon,
+  HStack,
+  Box,
+  useClipboard,
+} from '@chakra-ui/react';
+import { FiKey, FiChevronDown, FiCopy, FiXCircle } from 'react-icons/fi';
 
 const ConnectWallet = () => {
   const { connectWallet, isConnecting, isConnected, disconnect, address } =
     useWallet();
+  const copyAddress = useClipboard(address as string);
 
   return (
     <>
@@ -27,12 +39,35 @@ const ConnectWallet = () => {
         </Button>
       )}
       {isConnected && (
-        <>
-          <Text display="inline" fontSize="sm">
-            {formatAddress(address)}
-          </Text>
-          <Button onClick={() => disconnect()}>Disconnect</Button>
-        </>
+        <Menu offset={[0, 4]} placement="bottom-end">
+          <MenuButton
+            as={Button}
+            rightIcon={<Icon as={FiChevronDown} color="brand.primary.600" />}
+            color="brand.primary.600"
+            backgroundColor="brand.primary.50"
+            transition="all 100ms ease-in-out"
+            _hover={{ bgColor: 'brand.primary.100' }}
+            width="100%"
+          >
+            <Text color="gray.800" fontSize="sm">
+              {formatAddress(address)}
+            </Text>
+          </MenuButton>
+          <MenuList backgroundColor="gray.800" minWidth="none">
+            <MenuItem onClick={copyAddress.onCopy}>
+              <HStack spacing={2}>
+                <Icon as={FiCopy} />
+                <Box>{copyAddress.hasCopied ? 'Copied' : 'Copy Address'}</Box>
+              </HStack>
+            </MenuItem>
+            <MenuItem onClick={() => disconnect()}>
+              <HStack spacing={2}>
+                <Icon as={FiXCircle} color="red.300" />
+                <Box color="red.300">Sign Out</Box>
+              </HStack>
+            </MenuItem>
+          </MenuList>
+        </Menu>
       )}
     </>
   );
