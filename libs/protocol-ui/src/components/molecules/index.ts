486507428e2105856e5d1d0f381adeb4928cb405
jonathanprozzi <jonathanprozzi@gmail.com>
2022-05-23 00:20:52 -0400

    Added DatePicker component to the component library that leverages react-datepicker

--- libs/protocol-ui/src/components/molecules/index.ts
+++ libs/protocol-ui/src/components/molecules/index.ts
@@ -1,3 +1,5 @@
+export { default as DatePicker } from './DatePicker';
+export type { DatePickerProps } from './DatePicker';
 export { default as Input } from './Input';
 export type { InputProps } from './Input';
 export { default as ModalWrapper } from './ModalWrapper';
