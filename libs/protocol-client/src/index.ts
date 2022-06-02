ae16a9fe7c84e7ad0208db1657800fcd48db9d85
Alex Keating <keating.dev@protonmail.com>
2022-06-01 21:29:13 -0400

    Fix commented out

--- libs/protocol-client/src/index.ts
+++ libs/protocol-client/src/index.ts
@@ -1,3 +1,2 @@
 export * from './lib/protocol-client';
-// export { SortOrder, LinearIssueCreateManyInput } from './lib/protocol-types';
-export { SortOrder } from './lib/protocol-types';
+export * from './lib/protocol-types';
