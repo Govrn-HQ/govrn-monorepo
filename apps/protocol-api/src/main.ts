ecd2ea2a634404d146964578bcc17576d652309d
Keating <keating.dev@protonmail.com>
2022-06-12 09:59:17 -0400

    Initial permission rules

--- apps/protocol-api/src/main.ts
+++ apps/protocol-api/src/main.ts
@@ -2,17 +2,41 @@ import 'reflect-metadata';
 import { buildSchemaSync } from 'type-graphql';
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import { applyMiddleware } from 'graphql-middleware';
 import cors = require('cors');
 
 import { resolvers } from './prisma/generated/type-graphql';
-
+import { shield, deny, allow } from 'graphql-shield';
 import { graphqlHTTP } from 'express-graphql';
 
 const prisma = new PrismaClient();
 
-const schema = buildSchemaSync({
+const typeSchema = buildSchemaSync({
   resolvers,
 });
+
+const permissions = shield(
+  {
+    Query: {
+      '*': allow,
+    },
+    Attestation: {
+      '*': allow,
+    },
+    Contribution: {
+      '*': allow,
+    },
+    User: {
+      '*': allow,
+    },
+  },
+  {
+    fallbackRule: deny,
+    debug: true,
+  }
+);
+
+const schema = applyMiddleware(typeSchema, permissions);
 
 const app = express();
 app.use(cors());
