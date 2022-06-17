3c13ed78263a2eaacf4bdd466d65c619def2d032
jonathanprozzi <jonathanprozzi@gmail.com>
2022-06-17 00:59:59 -0400

    added localstorage support and logic for the mint flow

--- apps/protocol-frontend/src/components/MintModal.tsx
+++ apps/protocol-frontend/src/components/MintModal.tsx
@@ -8,6 +8,7 @@ import {
   Checkbox,
 } from '@chakra-ui/react';
 import { useUser } from '../contexts/UserContext';
+import { useLocalStorage } from '../utils/hooks';
 
 interface MintModalProps {
   contributions: any;
@@ -15,37 +16,62 @@ interface MintModalProps {
 
 const MintModal = ({ contributions }: MintModalProps) => {
   const { userData } = useUser();
+  const [isChecked, setIsChecked] = useState(false);
+  const [freshAgreementMint, setFreshAgreementMint] = useState(true);
+  const [agreementChecked, setAgreementChecked] = useLocalStorage(
+    'Govrn:Public-Data-Agreement',
+    {
+      agreement: false,
+    }
+  );
   const [minting, setMinting] = useState(false);
   const [currentContribution, setCurrentContribution] = useState(1);
 
-  console.log('contributions', contributions);
   const mintHandler = (contributions) => {
+    console.log('agreement: ', agreementChecked.agreement);
     setMinting(true);
     contributions.map((contribution, idx) => {
-      // mint logic
+      if (isChecked === true) {
+        setAgreementChecked((prevState: any) => ({
+          ...prevState,
+          agreement: true,
+        }));
+      }
       console.log(`contribution: ${idx}`, contribution.original);
       setMinting(false);
+      setFreshAgreementMint(false);
     });
   };
 
+  const agreementCheckboxHandler = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
-    <Stack spacing="4" width="100%" color="gray.800">
-      <Text paddingBottom={2} fontSize="sm">
+    <Stack spacing="3" width="100%" color="gray.800">
+      <Text fontSize="sm">
         Minting {contributions.length}{' '}
         {contributions.length === 1 ? 'Contribution' : 'Contributions'}
       </Text>
       <Text>
         Please note that minting will result in your Contribution data becoming
-        public. This means that anyone can see your Contribution data on Block
-        Explorers such as Etherscan.
+        public. This means that anyone will be able to see your Contribution
+        details on Block Explorers such as Etherscan (Mainnet) or Blockscout
+        (Gnosis Chain).
       </Text>
-      <Checkbox>
-        <Text color="black" fontWeight="normal" fontSize="md">
-          I understand
-        </Text>
-      </Checkbox>
+      <Text>
+        After you agree to the terms, we'll store this in your browser's local
+        storage and we'll ask you again when we update our protocol.
+      </Text>
+      {freshAgreementMint === true && !agreementChecked.agreement && (
+        <Checkbox onChange={agreementCheckboxHandler}>
+          <Text color="black" fontWeight="normal" fontSize="md">
+            I understand
+          </Text>
+        </Checkbox>
+      )}
       {minting ? <Progress color="brand.primary" /> : null}
-      <Flex align="flex-end" marginTop={4}>
+      <Flex align="flex-end" marginTop={8}>
         <Button
           type="submit"
           width="100%"
@@ -55,6 +81,7 @@ const MintModal = ({ contributions }: MintModalProps) => {
           _hover={{ bgColor: 'brand.primary.100' }}
           onClick={() => mintHandler(contributions)}
           isLoading={minting}
+          disabled={!isChecked}
         >
           Mint {contributions.length === 1 ? 'Contribution' : 'Contributions'}
         </Button>
