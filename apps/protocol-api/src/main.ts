675a29fc924ff73ccbce9d445de2b2b0a1774be5
Keating <keating.dev@protonmail.com>
2022-06-21 14:47:41 -0400

    Add another origin

--- apps/protocol-api/src/main.ts
+++ apps/protocol-api/src/main.ts
@@ -135,7 +135,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://beta.govrn.app'],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
