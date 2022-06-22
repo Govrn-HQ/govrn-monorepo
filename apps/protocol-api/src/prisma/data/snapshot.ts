c8ee409ba70bea5f1970632d92a358108ce6ce54
Amr Elghobary <amr.elghobary@gmail.com>
2022-06-22 23:15:18 +0200

    Add selector to snapshot for granular changes and type safety

--- apps/protocol-api/src/prisma/data/snapshot.ts
+++ apps/protocol-api/src/prisma/data/snapshot.ts
@@ -1,7 +1,7 @@
 import 'tslib';
 import * as fs from 'fs';
 import { Logger } from '@govrn-monorepo/utils';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 const writeCallback = (filename: string, err) => {
   if (err) {
@@ -17,98 +17,93 @@ const writeToFile = async (filename: string, data: any[]) => {
   });
 };
 
+const linearUserSelectWithNoRelation: Prisma.LinearUserSelect = {
+  active: true,
+  createdAt: true,
+  displayName: true,
+  email: true,
+  linear_id: true,
+  name: true,
+  url: true,
+};
+
+const linearIssueSelectWithNoRelation: Prisma.LinearIssueSelect = {
+  archivedAt: true,
+  autoArchivedAt: true,
+  autoClosedAt: true,
+  boardOrder: true,
+  branchName: true,
+  canceledAt: true,
+  completedAt: true,
+  createdAt: true,
+  customerTickerCount: true,
+  description: true,
+  dueDate: true,
+  estimate: true,
+  linear_id: true,
+  identifier: true,
+  priority: true,
+  priorityLabel: true,
+  snoozedUntilAt: true,
+  sortOrder: true,
+  startedAt: true,
+  subIssueSortOrder: true,
+  title: true,
+  trashed: true,
+  updatedAt: true,
+  url: true,
+};
+
+const linearCycleSelect: Prisma.LinearCycleSelect = {
+  number: true,
+  startsAt: true,
+  endsAt: true,
+  linear_id: true,
+};
+
+const linearTeamSelect: Prisma.LinearTeamSelect = {
+  linear_id: true,
+  name: true,
+  key: true,
+};
+
+const linearProjectSelect: Prisma.LinearProjectSelect = {
+  name: true,
+  linear_id: true,
+};
+
+const linearIssueSelect: Prisma.LinearIssueSelect = {
+  ...linearIssueSelectWithNoRelation,
+  creator: { select: linearUserSelectWithNoRelation },
+  assignee: { select: linearUserSelectWithNoRelation },
+  cycle: { select: linearCycleSelect },
+  project: { select: linearProjectSelect },
+  team: { select: linearTeamSelect },
+};
+
+const linearUserSelect: Prisma.LinearUserSelect = {
+  active: true,
+  createdAt: true,
+  displayName: true,
+  email: true,
+  linear_id: true,
+  name: true,
+  url: true,
+  assigned_issues: {
+    select: linearIssueSelect,
+  },
+  created_issues: {
+    select: linearIssueSelect,
+  },
+};
+
 const main = async () => {
   console.log('\n:: Collecting Started\n');
 
-  const client = new PrismaClient();
+  const prisma = new PrismaClient({ errorFormat: 'pretty' });
 
-  const linearIssueSelect = {
-    archivedAt: true,
-    autoArchivedAt: true,
-    autoClosedAt: true,
-    boardOrder: true,
-    branchName: true,
-    canceledAt: true,
-    completedAt: true,
-    createdAt: true,
-    customerTickerCount: true,
-    description: true,
-    dueDate: true,
-    estimate: true,
-    linear_id: true,
-    identifier: true,
-    priority: true,
-    priorityLabel: true,
-    snoozedUntilAt: true,
-    sortOrder: true,
-    startedAt: true,
-    subIssueSortOrder: true,
-    title: true,
-    trashed: true,
-    updatedAt: true,
-    url: true,
-    creator: {
-      select: {
-        active: true,
-        createdAt: true,
-        displayName: true,
-        email: true,
-        linear_id: true,
-        name: true,
-        url: true,
-      },
-    },
-    assignee: {
-      select: {
-        active: true,
-        createdAt: true,
-        displayName: true,
-        email: true,
-        linear_id: true,
-        name: true,
-        url: true,
-      },
-    },
-    cycle: {
-      select: {
-        number: true,
-        startsAt: true,
-        endsAt: true,
-        linear_id: true,
-      },
-    },
-    project: {
-      select: {
-        name: true,
-        linear_id: true,
-      },
-    },
-    team: {
-      select: {
-        linear_id: true,
-        name: true,
-        key: true,
-      },
-    },
-  };
-
-  const linearUsers = await client.linearUser.findMany({
-    select: {
-      active: true,
-      createdAt: true,
-      displayName: true,
-      email: true,
-      linear_id: true,
-      name: true,
-      url: true,
-      // TODO: add user,
-      assigned_issues: {
-        select: linearIssueSelect,
-      },
-      created_issues: {
-        select: linearIssueSelect,
-      },
-    },
+  const linearUsers = await prisma.linearUser.findMany({
+    select: linearUserSelect,
   });
 
   await writeToFile('linear-snapshot.json', linearUsers);
