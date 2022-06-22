9d2a6ad268ea1451d10df72298728acf54d82dd6
jonathanprozzi <jonathanprozzi@gmail.com>
2022-06-21 20:39:45 -0400

    added stream-browserify

--- apps/protocol-frontend/vite.config.ts
+++ apps/protocol-frontend/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import strip from '@rollup/plugin-strip';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { viteExternalsPlugin } from 'vite-plugin-externals';
 
@@ -12,12 +13,21 @@ const externalPlugin = viteExternalsPlugin({
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    strip(),
     tsconfigPaths({
       root: '../..',
     }),
     externalPlugin,
   ],
+  resolve: {
+    alias: {
+      stream: 'stream-browserify',
+    },
+  },
   build: {
     target: 'esnext',
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
   },
 });
