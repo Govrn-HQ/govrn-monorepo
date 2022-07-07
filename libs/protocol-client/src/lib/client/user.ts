c58353a171eb980b79f7a0ca12cb4586e2f5101e
Amr Elghobary <12991700+amrro@users.noreply.github.com>
2022-07-07 18:22:59 +0200

    Update contract sync job to load off-chain details (#79)
    Add contract sync job changes

--- libs/protocol-client/src/lib/client/user.ts
+++ libs/protocol-client/src/lib/client/user.ts
@@ -1,10 +1,11 @@
-import { UserUpdateInput, UserWhereUniqueInput } from '../protocol-types';
-import { BaseClient } from './base';
 import {
-  UserCreateCustomInput,
   ListUsersQueryVariables,
   MutationDeleteGuildUserArgs,
+  UserCreateCustomInput,
+  UserUpdateInput,
+  UserWhereUniqueInput,
 } from '../protocol-types';
+import { BaseClient } from './base';
 import { GraphQLClient } from 'graphql-request';
 
 export class User extends BaseClient {
@@ -31,7 +32,6 @@ export class User extends BaseClient {
   }
 
   public async create(args: UserCreateCustomInput) {
-    console.log(this.sdk);
     const contributions = await this.sdk.createUserCustom({ data: args });
     return contributions.createUserCustom;
   }
