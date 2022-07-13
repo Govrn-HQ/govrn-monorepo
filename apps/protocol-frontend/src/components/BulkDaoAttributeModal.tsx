7c490cdcd732b379e6e53121ce9d39958238ccab
jonathanprozzi <jonathanprozzi@gmail.com>
2022-07-13 10:26:14 -0400

    added scaffolding for the bulk attributing to daos -- added form, modal, and updated the context to support the modal

---
+++ apps/protocol-frontend/src/components/BulkDaoAttributeModal.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import { Stack, Flex, Button, Text, Progress } from '@chakra-ui/react';
+import { useUser } from '../contexts/UserContext';
+
+interface BulkDaoAttributeModalProps {
+  contributions: any;
+}
+
+const BulkDaoAttributeModal = ({
+  contributions,
+}: BulkDaoAttributeModalProps) => {
+  const { userData, createAttestation, mintAttestation } = useUser();
+  const [attesting, setAttesting] = useState(false);
+  const [currentAttestation, setCurrentAttestation] = useState(1);
+
+  const createAttestationsHandler = (contributions) => {
+    setAttesting(true);
+    contributions.map((contribution, idx) => {
+      console.log(`contribution: ${idx}`, contribution.original);
+      if (contribution.original.status === 'minted') {
+        console.log(`contribution ${idx} is on chain`);
+        mintAttestation(contribution.original);
+      } else {
+        console.log(`contribution ${idx} is off chain`);
+        createAttestation(contribution.original);
+      }
+      setAttesting(false);
+    });
+  };
+
+  return (
+    <Stack spacing="4" width="100%" color="gray.800">
+      <Text paddingBottom={2}>Attesting as: {userData.name}</Text>
+      <Text paddingBottom={2}>
+        Attesting to {contributions.length}{' '}
+        {contributions.length === 1 ? 'Contribution' : 'Contributions'}
+      </Text>
+      {attesting ? (
+        <Progress
+          color="brand.primary"
+          value={currentAttestation % contributions.length}
+        />
+      ) : null}
+      <Flex align="flex-end" marginTop={4}>
+        <Button
+          type="submit"
+          width="100%"
+          color="brand.primary.600"
+          backgroundColor="brand.primary.50"
+          transition="all 100ms ease-in-out"
+          _hover={{ bgColor: 'brand.primary.100' }}
+          onClick={() => createAttestationsHandler(contributions)}
+          isLoading={attesting}
+        >
+          Add {contributions.length === 1 ? 'Attestation' : 'Attestations'}
+        </Button>
+      </Flex>
+    </Stack>
+  );
+};
+
+export default BulkDaoAttributeModal;
