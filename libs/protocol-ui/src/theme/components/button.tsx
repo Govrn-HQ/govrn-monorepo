9e116c73baa0139739fbd47b7bf2b903308aa20b
Nicholas Koech <nicholask320@gmail.com>
2022-07-18 19:11:22 -0700

    Add disable to ingration buttons (#88)
    * Grey out integration buttons

---
+++ libs/protocol-ui/src/theme/components/button.tsx
@@ -0,0 +1,16 @@
+export default {
+    variants: {
+      disabled: {
+        _disabled: {
+          bgColor: 'gray.300',
+          color: 'gray.800',
+        },
+        _hover: {
+          _disabled: {
+            bgColor: 'gray.300',
+          },
+        },
+      },
+    },
+  };
+  
