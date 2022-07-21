a706c128527a157b95e7939607b4489256942472
jonathanprozzi <jonathanprozzi@gmail.com>
2022-07-21 11:51:21 -0400

    streamlined the activityType error message and made it nullable

--- apps/protocol-frontend/src/utils/validations.ts
+++ apps/protocol-frontend/src/utils/validations.ts
@@ -44,12 +44,9 @@ export const editContributionFormValidation = yup.object({
 });
 
 export const reportFormValidation = yup.object({
-  name: yup.string().required('This field is required.'),
-  engagementDate: yup.date(),
-  activityType: yup.object().shape({
-    label: yup.string().required('Activity Type is required.'),
-    value: yup.string().required('Activity Type is required.'),
-  })
+  name: yup.string().required('Contribution Name is required.'),
+  engagementDate: yup.date().required('Engagement Date is required.'),
+  activityType: yup.string().nullable().required('Activity Type is required.'),
 });
 
 export const addAttestationFormValidation = yup.object({
