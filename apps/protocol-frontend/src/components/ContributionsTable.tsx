c2eb1d76d6a6d649d37e738d56a6f1d73c710c02
Nkoech123 <nicholask320@gmail.com>
2022-07-25 11:24:20 -0700

    guildName line 62 simplified

--- apps/protocol-frontend/src/components/ContributionsTable.tsx
+++ apps/protocol-frontend/src/components/ContributionsTable.tsx
@@ -59,7 +59,7 @@ const ContributionsTable = ({
         activityTypeId: contribution.activity_type.id,
         status: contribution.status.name,
         action: '',
-        guildName: !contribution.guilds.map((guildObj: any) => guildObj.guild.name)[0]? '---' : contribution.guilds.map((guildObj: any) => guildObj.guild.name)[0]
+        guildName: contribution.guilds.map((guildObj: any) => guildObj.guild.name)[0]?? '---' ,
 
       })),
     [contributionsData]
@@ -67,18 +67,7 @@ const ContributionsTable = ({
  
   const columns = useMemo(
     () => [
-      {
-        Header: 'DAO',
-        accessor: 'guildName',
-        Cell: ({ value }) => {
-          return (
-            // <Stack direction="row">
-            //   <Checkbox size="lg" />
-            <Text>{value}</Text>
-            // </Stack>
-          );
-        },
-      },
+   
       {
         Header: 'Name',
         accessor: 'name',
@@ -117,6 +106,19 @@ const ContributionsTable = ({
         accessor: 'attestations',
         Cell: ({ value }) => {
           return <Text textTransform="capitalize">{value.length} </Text>;
+        },
+      },
+
+      {
+        Header: 'DAO',
+        accessor: 'guildName',
+        Cell: ({ value }) => {
+          return (
+            // <Stack direction="row">
+            //   <Checkbox size="lg" />
+            <Text>{value}</Text>
+            // </Stack>
+          );
         },
       },
     ],
