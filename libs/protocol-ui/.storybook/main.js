2c1c6d67778b5b865f5311109690d2f8e14c1422
Jonathan Prozzi <jonathanprozzi@gmail.com>
2022-07-25 21:04:52 -0400

    feature/component-library-docs-pro-218 (#111)
    * Add initial storybook stories

--- libs/protocol-ui/.storybook/main.js
+++ libs/protocol-ui/.storybook/main.js
@@ -7,10 +7,17 @@ module.exports = {
 
   stories: [
     ...rootMain.stories,
-    '../src/lib/**/*.stories.mdx',
-    '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/stories/index.stories.tsx',
+    '../src/stories/**/*.stories.@(ts|tsx|js|jsx)',
   ],
-  addons: [...rootMain.addons, '@nrwl/react/plugins/storybook'],
+  addons: [
+    ...rootMain.addons,
+    '@nrwl/react/plugins/storybook',
+    '@chakra-ui/storybook-addon',
+  ],
+  features: {
+    emotionAlias: false,
+  },
   webpackFinal: async (config, { configType }) => {
     // apply any global webpack configs that might have been specified in .storybook/main.js
     if (rootMain.webpackFinal) {
---
+++ libs/protocol-ui/.storybook/preview.js
@@ -0,0 +1,7 @@
+import GovrnTheme from '../src/theme/index';
+
+export const parameters = {
+  chakra: {
+    GovrnTheme,
+  },
+};
