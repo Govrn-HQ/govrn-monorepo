2e941a56258021f708b0429064fdfdb26f630742
Amr Elghobary <12991700+amrro@users.noreply.github.com>
2022-07-28 18:37:42 +0200

    Build Endpoint to load Attestations in JSON-LD schema (#110)
    * Add avenue service and endpoint

---
+++ apps/voyager-identity-api/src/subgraph.ts
@@ -0,0 +1,55 @@
+import { GraphQLClient } from 'graphql-request';
+import { GovrnGraphClient } from '@govrn/govrn-subgraph-client';
+import { GovrnContract, NetworkConfig } from '@govrn/govrn-contract-client';
+import { BigNumber, ethers } from 'ethers';
+import { LDContribution } from './types';
+
+// Environment Variables.
+const SUBGRAPH_ENDPOINT = process.env.SUBGRAPH_URL;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const CHAIN_URL = process.env.CHAIN_URL;
+
+const LIMIT = 100;
+
+const networkConfig: NetworkConfig = {
+  address: CONTRACT_ADDRESS,
+  chainId: 100,
+};
+
+const provider = new ethers.providers.JsonRpcProvider(CHAIN_URL);
+const govrnContract = new GovrnContract(networkConfig, provider);
+const graphQLClient = new GraphQLClient(SUBGRAPH_ENDPOINT);
+const client = new GovrnGraphClient(graphQLClient);
+
+export const loadContributions = async (options: {
+  page?: number;
+  limit?: number;
+}): Promise<LDContribution[]> => {
+  // Load attestations events from subgraph.
+  const page = options.page || 0; // || for NaN value.
+  const limit = options.limit || LIMIT;
+
+  const events = (
+    await client.listAttestations({
+      skip: page * limit,
+      first: limit,
+    })
+  ).attestations;
+
+  // Load corresponding contributions to get `detailsUri`.
+  const contrs = await Promise.all(
+    events.map(async (e) => {
+      const contr = await govrnContract.contributions({
+        tokenId: e.contribution.id,
+      });
+      return { ...contr, ...e };
+    })
+  );
+
+  return contrs.map((c) => ({
+    id: Number(BigNumber.from(c.contribution.id.toString()).toString()),
+    attestor: c.attestor,
+    owner: c.owner,
+    detailsUri: ethers.utils.toUtf8String(c.detailsUri),
+  }));
+};
---
+++ apps/voyager-identity-api/src/types.ts
@@ -0,0 +1,6 @@
+export type LDContribution = {
+  id: number;
+  attestor: string;
+  owner: string;
+  detailsUri: string;
+};
