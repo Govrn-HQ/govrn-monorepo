7352bbcc76d969b79b16e0f40b57d16513a9198a
Nkoech123 <nicholask320@gmail.com>
2022-07-31 04:50:51 -0700

    Molecules Stories

--- libs/protocol-ui/src/stories/molecules/ModalWrapper.stories.tsx
+++ libs/protocol-ui/src/stories/molecules/ModalWrapper.stories.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
 import { Story } from '@storybook/react';
-import { ModalWrapper, ModalWrapperProps} from '../..';
+import React from 'react';
+import { ModalWrapper, ModalWrapperProps } from '../../';
+// import { Modal } from '@chakra-ui/react';
 
 export default {
-    title: 'Components/Molecules/ModalWrapper',
-    component: ModalWrapper,
-    argTypes: {
-        tipText: {
-            control: {type: 'text'},
-        },
-    },
-}
+  title: 'Components/Molecules/ModalWrapper',
+  component: ModalWrapper,
+};
+
+const Template: Story<ModalWrapperProps> = args => <ModalWrapper {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  name: 'name',
+  title: 'Modal Title',
+  content: 'This is the content of this Modal',
+  isOpen: false,
+  size: '2xl',
+  titleColor: 'gray',
+  bgColor: 'gray',
+  localOverlay: 'Modal Overlay',
+};
