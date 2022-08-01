436af72a8236dd531d76b404cbd5a43d3a873919
Nicholas Koech <nicholask320@gmail.com>
2022-08-01 06:35:56 -0700

    Atom stories (#117)
    * Add form atoms
    
    Co-authored-by: jonathanprozzi <jonathanprozzi@gmail.com>

---
+++ libs/protocol-ui/src/stories/atoms/FormLabel.stories.tsx
@@ -0,0 +1,24 @@
+import { Story } from '@storybook/react';
+import React from 'react';
+import { FormLabel, FormLabelProps } from '../../';
+
+export default {
+  title: 'Components/Atoms/FormLabel',
+  component: FormLabel,
+  argTypes: {
+    label: {
+      control: { type: 'text' },
+    },
+    color: { control: 'color' },
+  },
+};
+
+const Template: Story<FormLabelProps> = (args) => <FormLabel {...args} />;
+
+const Default = Template.bind({});
+Default.args = {
+  label: 'DAO',
+  color: 'gray.800',
+};
+
+export { Default };
