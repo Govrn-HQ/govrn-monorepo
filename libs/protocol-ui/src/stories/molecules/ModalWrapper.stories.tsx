b18fde88db583a8a80b868af0d104b5b5238ce02
Nkoech123 <nicholask320@gmail.com>
2022-08-02 12:14:03 -0700

    updated Textarea args

--- libs/protocol-ui/src/stories/molecules/ModalWrapper.stories.tsx
+++ libs/protocol-ui/src/stories/molecules/ModalWrapper.stories.tsx
@@ -1,7 +1,8 @@
 import { Story } from '@storybook/react';
 import React from 'react';
 import { ModalWrapper, ModalWrapperProps } from '../../';
-// import { Modal } from '@chakra-ui/react';
+import { Textarea } from '../../components/molecules/Textarea/Textarea';
+//import {useOverlay} from '../../../../../apps/protocol-frontend/src/contexts/OverlayContext';
 
 export default {
   title: 'Components/Molecules/ModalWrapper',
@@ -13,9 +14,15 @@ const Template: Story<ModalWrapperProps> = args => <ModalWrapper {...args} />;
 export const Default = Template.bind({});
 
 Default.args = {
-  name: 'name',
-  title: 'Modal Title',
-  content: 'This is the content of this Modal',
+  name: 'TextareaModal',
+  title: 'Describe Contribution',
+  content: (
+    <Textarea
+      name="name"
+      label="Textarea label"
+      placeholder="Describe contribution"
+    />
+  ),
   isOpen: false,
   size: '2xl',
   titleColor: 'gray',
