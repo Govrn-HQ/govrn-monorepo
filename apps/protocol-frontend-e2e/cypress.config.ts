c06c05fb7d461a52a0d9c3a09c16d38b97b46781
Jonathan Prozzi <jonathanprozzi@gmail.com>
2022-08-07 21:46:13 -0400

    feature/frontend-add-cypress (PRO-295) (#142)
    * Add cypress for protocol-frontend

---
+++ apps/protocol-frontend-e2e/cypress.config.ts
@@ -0,0 +1,17 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  fileServerFolder: '.',
+  fixturesFolder: './src/fixtures',
+  modifyObstructiveCode: false,
+  video: true,
+  videosFolder: '../../dist/cypress/apps/protocol-frontend-e2e/videos',
+  screenshotsFolder:
+    '../../dist/cypress/apps/protocol-frontend-e2e/screenshots',
+  chromeWebSecurity: false,
+  e2e: {
+    setupNodeEvents(on, config) { },
+    specPattern: './src/integration/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: './src/support/index.ts',
+  },
+});
