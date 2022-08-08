2a6d4e5bf5c80380e2cfaff5d8dacc606a264a52
Alex Keating <keating.dev@protonmail.com>
2022-08-07 22:17:47 -0400

    Fix cypress linting

--- apps/protocol-frontend-e2e/cypress.config.ts
+++ apps/protocol-frontend-e2e/cypress.config.ts
@@ -1,16 +1,18 @@
-const { defineConfig } = require('cypress');
+import { defineConfig } from 'cypress';
 
 module.exports = defineConfig({
   fileServerFolder: '.',
   fixturesFolder: './src/fixtures',
-  modifyObstructiveCode: false,
+  // modifyObstructiveCode: false,
   video: true,
   videosFolder: '../../dist/cypress/apps/protocol-frontend-e2e/videos',
   screenshotsFolder:
     '../../dist/cypress/apps/protocol-frontend-e2e/screenshots',
   chromeWebSecurity: false,
   e2e: {
-    setupNodeEvents(on, config) { },
+    setupNodeEvents(on, config) {
+      return;
+    },
     specPattern: './src/integration/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: './src/support/index.ts',
   },
