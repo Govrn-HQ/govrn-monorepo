dcca4ae3bdb181063041b39eed58f4a314a35bbb
jonathanprozzi <jonathanprozzi@gmail.com>
2022-08-12 14:43:59 -0400

    fixed Select and CreatableSelect stories

--- libs/protocol-ui/src/stories/molecules/CreatableSelect.stories.tsx
+++ libs/protocol-ui/src/stories/molecules/CreatableSelect.stories.tsx
@@ -1,22 +1,75 @@
-import { Story } from '@storybook/react';
 import React from 'react';
-import { CreatableSelect, CreatableSelectProps} from '../../';
-
+import { useForm } from 'react-hook-form';
+import { CreatableSelect, CreatableSelectProps } from '../..';
 
 export default {
-    title: 'Components/Molecules/CreatableSelect',
-    component: CreatableSelect,
-  
-}
-
-const Template: Story<CreatableSelectProps> = (args) => <CreatableSelect {...args} />;
-
-
-export const Default = Template.bind({});
-
-Default.args = {
-    label: 'Select options below',
-    placeholder: 'Select options',
-    options: [{label:'Govrn',value: 'Govrn'},{label:'MGD',value: 'MGD'}], 
-    
+  title: 'Components/Molecules/CreatableSelect',
+  component: CreatableSelect,
 };
+
+const sampleOptions = [
+  { value: 'Pull Request1', label: 'Pull Request' },
+  { value: 'Documentation', label: 'Documentation' },
+  { value: 'Note Taking', label: 'Note Taking' },
+  { value: 'Design', label: 'Design' },
+  { value: 'Other', label: 'Other' },
+];
+
+const Single = (args: CreatableSelectProps) => {
+  const localForm = useForm();
+  return (
+    <form>
+      <CreatableSelect {...args} localForm={localForm} />
+    </form>
+  );
+};
+
+const Multi = (args: CreatableSelectProps) => {
+  const localForm = useForm();
+  return (
+    <form>
+      <CreatableSelect {...args} localForm={localForm} />
+    </form>
+  );
+};
+
+const WithTip = (args: CreatableSelectProps) => {
+  const localForm = useForm();
+  return (
+    <form>
+      <CreatableSelect {...args} localForm={localForm} />
+    </form>
+  );
+};
+
+Single.args = {
+  name: 'activityType',
+  label: 'Activity Type',
+  placeholder:
+    'Choose an Activity Type from the list or start typing your own.',
+  variant: 'filled',
+  options: sampleOptions,
+};
+
+Multi.args = {
+  name: 'activityType',
+  label: 'Activity Type',
+  placeholder:
+    'Choose an Activity Type from the list or start typing your own.',
+  variant: 'filled',
+  options: sampleOptions,
+  isMulti: true,
+};
+
+WithTip.args = {
+  name: 'activityType',
+  label: 'Activity Type',
+  placeholder:
+    'Choose an Activity Type from the list or start typing your own.',
+  tip: 'Select DAO(s) to associate this Contribution with.',
+  variant: 'filled',
+  options: sampleOptions,
+  isMulti: true,
+};
+
+export { Single, Multi, WithTip };
