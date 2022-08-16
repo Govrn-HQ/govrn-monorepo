95c91a0ce9530236502dc1935e635c23fe2cfae7
Scott Herren <me@stwoh.com>
2022-08-16 13:43:02 -0500

    polyfill-buffer

--- apps/protocol-frontend/src/polyfills.ts
+++ apps/protocol-frontend/src/polyfills.ts
@@ -5,3 +5,10 @@
  */
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import { Buffer } from 'buffer';
+
+window.global = window.global ?? window;
+window.Buffer = window.Buffer ?? Buffer;
+// window.process = window.process ?? { env: {} }; // Minimal process polyfill
+
+// export {};
