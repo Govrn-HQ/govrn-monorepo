64b0b8a9ee0f078cdf3d77b78446931edfc9091f
Scott Herren <me@stwoh.com>
2022-08-30 11:34:46 -0500

    preserve polyfill comment

--- apps/protocol-frontend/src/polyfills.ts
+++ apps/protocol-frontend/src/polyfills.ts
@@ -1,7 +1,12 @@
 import { Buffer } from 'buffer';
 
-
 window.Buffer = window.Buffer ?? Buffer;
 window.process = window.process ?? { env: {} }; // Minimal process polyfill
 
-export { };
+export {};
+
+/*
+// If we see issues with 'regenerator-runtime', add back these imports
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+*/
