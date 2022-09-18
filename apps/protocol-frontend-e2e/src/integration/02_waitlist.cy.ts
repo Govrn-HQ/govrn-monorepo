a9d0ec4e2df8b19646c7f07ffbc4793cfd6eaaf5
Nicholas Koech <nicholask320@gmail.com>
2022-09-18 16:46:12 -0700

    Initial Cypress tests (#183)
    * Add first cypress tests
    
    Co-authored-by: Keating <keating.dev@protonmail.com>

---
+++ apps/protocol-frontend-e2e/src/integration/02_waitlist.cy.ts
@@ -0,0 +1,48 @@
+/// <reference types="cypress" />
+
+const insertDAOs = `
+INSERT INTO "Guild" (id, name)
+    VALUES (1,'Govrn')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO "Guild" (id, name)
+    VALUES (2,'MGD')
+ON CONFLICT DO NOTHING;
+`;
+beforeEach(() => {
+  cy.task('queryDatabase', insertDAOs);
+
+  cy.fixture('users.json').then(users => {
+    this.users = users;
+  });
+
+  cy.fixture('testaccounts.json').then(accounts => {
+    this.accounts = accounts;
+    cy.login(this.accounts[0].address, this.accounts[0].privateKey);
+  });
+});
+
+afterEach(() => {
+  cy.login(this.accounts[0].address, this.accounts[0].privateKey);
+});
+
+describe('Join Waitlist', () => {
+  it('Fill Govrn Waitlist Form', () => {
+    cy.get('[data-cy="create-my-profile-btn"]', { timeout: 60000 })
+      .should('be.visible')
+      .click({ force: true });
+
+    cy.get('input[data-testid="chakraInput-test"]', { timeout: 20000 })
+      .should('be.enabled')
+      .eq(0)
+      .type(this.users[0].username)
+      .should('have.value', this.users[0].username);
+
+    cy.get('input[data-testid="chakraInput-test"]')
+      .eq(1)
+      .type(this.users[0].email)
+      .should('have.value', this.users[0].email);
+
+    cy.get('[data-cy="join-waitlist"]').click();
+  });
+});
