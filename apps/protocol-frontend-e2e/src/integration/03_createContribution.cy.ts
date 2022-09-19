16d557765badd140763eef58d467a0a872623eaf
Keating <keating.dev@protonmail.com>
2022-09-18 23:45:35 -0400

    Increase timeouts

--- apps/protocol-frontend-e2e/src/integration/03_createContribution.cy.ts
+++ apps/protocol-frontend-e2e/src/integration/03_createContribution.cy.ts
@@ -5,64 +5,60 @@ const getUserOffWaitlistQuery = `
   SET active = TRUE
   WHERE email = 'testemail@gmail.com'
   RETURNING *;
-  `
-before(()=>{
-   //Get user off the waitlist
-  cy.task('queryDatabase', getUserOffWaitlistQuery)
-    .then((res) => {
+  `;
+before(() => {
+  //Get user off the waitlist
+  cy.task('queryDatabase', getUserOffWaitlistQuery).then(res => {
     expect(res.rows[0].active).to.equal(true);
   });
 
-  cy.fixture('contributions.json').then((contributions) => {
-    this.contributions = contributions
+  cy.fixture('contributions.json').then(contributions => {
+    this.contributions = contributions;
   });
 
-  cy.fixture('testaccounts.json').then((accounts) => {
-    this.accounts = accounts
+  cy.fixture('testaccounts.json').then(accounts => {
+    this.accounts = accounts;
     cy.login(this.accounts[0].address, this.accounts[0].privateKey);
   });
 
-  cy.get('[data-cy="myContributions-btn"]', {timeout:10000})
+  cy.get('[data-cy="myContributions-btn"]', { timeout: 10000 })
     .should('be.visible')
-    .click({force:true});
-
+    .click({ force: true });
 });
 
-describe("Create First Contribution", () => {
-
+describe('Create First Contribution', () => {
   it('Report your first Contribution', () => {
+    cy.get('[data-cy="reportFirstContribution-btn"]', {
+      timeout: 10000,
+    }).click();
 
-    cy.get('[data-cy="reportFirstContribution-btn"]' , { timeout: 10000 })
-      .click();
-      
-    cy.get('input[data-testid="chakraInput-test"]')
+    cy.get('input[data-testid="chakraInput-test"]', { timeout: 10000 })
+      .should('be.visible')
       .eq(0)
       .type(this.contributions[0].name)
       .should('have.value', this.contributions[0].name);
-  
-    cy.get(".css-ujecln-Input2 #react-select-3-input")
-      .type(`${this.contributions[0].activityType}{enter}`);
-    
+
+    cy.get('.css-ujecln-Input2 #react-select-3-input').type(
+      `${this.contributions[0].activityType}{enter}`,
+    );
+
     cy.get('textarea[data-testid="textarea-test"]')
-      .click() 
-      .type(this.contributions[0].details);   
-    
+      .click()
+      .type(this.contributions[0].details);
+
     cy.get('input[data-testid="chakraInput-test"]')
       .eq(1)
-      .type(this.contributions[0].proof); 
-        
-    cy.get(".css-ujecln-Input2 #react-select-5-input") 
-      .click({ force: true})
+      .type(this.contributions[0].proof);
+
+    cy.get('.css-ujecln-Input2 #react-select-5-input')
+      .click({ force: true })
       .type(`${this.contributions[0].dao}{enter}`);
-        
-    cy.get('[data-cy="addContribution-btn"]')
-      .click({ force: true }) 
 
-    cy.contains('Please select at least one Contribution to attribute to a DAO or mint.', 
-       {timeout: 10000 });
+    cy.get('[data-cy="addContribution-btn"]').click({ force: true });
 
+    cy.contains(
+      'Please select at least one Contribution to attribute to a DAO or mint.',
+      { timeout: 10000 },
+    );
   });
-
- 
 });
-   
