e47cc53f9950372d247f5329753a59ff63f88d6f
Alexander Keating <keating.dev@protonmail.com>
2022-09-21 12:16:53 -0400

    Revert "Revert " Add Pagination to Contributions Table (#205)""
    This reverts commit cabd0e0381052d6861cffa85018fa56e626df33f.

--- apps/protocol-frontend/src/utils/constants.ts
+++ apps/protocol-frontend/src/utils/constants.ts
@@ -1,5 +1,6 @@
-export const GOVRN_MOTTO =
-  'Track and record your DAO Contributions.';
+/* Environment Variables */
+
+export const PROTOCOL_URL = import.meta.env.VITE_PROTOCOL_URL;
 export const BASE_URL = import.meta.env.VITE_PROTOCOL_BASE_URL;
 
 export const VERIFY_URL = `${BASE_URL}/verify`;
@@ -9,4 +10,7 @@ export const BLOCK_EXPLORER_URLS = {
   gnosisChain: `https://blockscout.com/xdai/mainnet/tx/`,
 };
 
-export const UNASSIGNED = 'Unassigned'
+/* Project Related Constants */
+
+export const UNASSIGNED = 'Unassigned';
+export const GOVRN_MOTTO = 'Track and record your DAO Contributions.';
