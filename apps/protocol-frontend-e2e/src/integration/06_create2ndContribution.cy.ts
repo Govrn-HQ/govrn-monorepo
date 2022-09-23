1267d3523bec969599a2dd76d58dd0c0373887ca
Nkoech123 <nicholask320@gmail.com>
2022-09-23 04:06:55 -0700

    fix flakiness

--- apps/protocol-frontend-e2e/src/integration/06_create2ndContribution.cy.ts
+++ apps/protocol-frontend-e2e/src/integration/06_create2ndContribution.cy.ts
@@ -1,17 +1,6 @@
 /// <reference types="cypress" />
 
-const getUserOffWaitlistQuery = `
-  UPDATE "User"
-  SET active = TRUE
-  WHERE email = 'testemail@gmail.com'
-  RETURNING *;
-  `
   before(() => {
-    //Get user off the waitlist
-    cy.task('queryDatabase', getUserOffWaitlistQuery).then(res => {
-      expect(res.rows[0].active).to.equal(true);
-    });
-  
     cy.fixture('contributions.json').then(contributions => {
       this.contributions = contributions;
     });
@@ -21,49 +10,53 @@ const getUserOffWaitlistQuery = `
       cy.login(this.accounts[0].address, this.accounts[0].privateKey);
     });
   
-    cy.get('[data-cy="myContributions-btn"]', { timeout: 10000 })
+    cy.get('[data-cy="myContributions-btn"]', { timeout: 15000 })
       .should('be.visible')
       .click({ force: true });
+
+    cy.get('[data-testid="floatingreportbtn-testid"]', {
+        timeout: 10000,
+      }).should('be.visible')
+        .click();
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(5000)
   });
 
 describe("Create Second Contribution", () => {
-
   it('Report your Second Contribution', () => {
     const contribution = this.contributions[2];
-    
-    cy.get('[data-testid="floatingreportbtn-testid"]', {
-      timeout: 10000,
-    }).click();
-    cy.wait(3000);
-    cy.get('input[data-testid="reportForm-name"]') 
+
+    cy.get('input[data-testid="reportForm-name"]', {timeout:20000})  
+      .should('be.visible')
       .type(contribution.name)
       .should('have.value', contribution.name);
-    
-    cy.get(".css-ujecln-Input2 #react-select-7-input")
-      .click({ force: true})
-      .type(`${contribution.activityType}{enter}`);
-   
+
+    cy.get('.css-ujecln-Input2')
+      .should('be.visible')
+      .eq(0)
+      .children()
+      .eq(0)
+      .type(`${contribution.activityType}{enter}`,
+    );
+
     cy.get('textarea[data-testid="textarea-test"]')
-      .click() 
-      .type(contribution.details);   
-    
-    cy.get('input[data-testid="reportForm-proof"]')
-      .type(contribution.proof);   
-        
-    cy.get(".css-ujecln-Input2 #react-select-9-input") 
-      .click({ force: true})
+      .click()
+      .type(contribution.details);
+
+    cy.get('input[data-testid="reportForm-proof"]').type(contribution.proof);
+
+    cy.get('.css-ujecln-Input2')
+      .should('be.visible')
+      .eq(1)
+      .children()
+      .eq(0)
       .type(`${contribution.dao}{enter}`);
         
-    cy.get('[data-cy="addContribution-btn"]')
-      .click();
+    cy.get('[data-cy="addContribution-btn"]').click();
 
     cy.contains(
       'Please select at least one Contribution to attribute to a DAO or mint.',
-      { timeout: 10000 },
+      { timeout: 20000 },
     );
-      
-
   });
-
- 
 });
