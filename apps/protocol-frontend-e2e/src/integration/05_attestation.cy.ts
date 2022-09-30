f6044011434f19482b01b1fc30794963e8072396
Nicholas Koech <nicholask320@gmail.com>
2022-09-30 12:23:25 -0700

    added test 05

---
+++ apps/protocol-frontend-e2e/src/integration/05_attestation.cy.ts
@@ -0,0 +1,67 @@
+/// <reference types="cypress" />
+
+beforeEach(() => {
+    cy.teardownDB(["ContributionStatus"]) //in case of flicker
+    const tableNamesOrActions = ["Guild", "LoginUser1", "LoginUser2", "GuildUser1","GuildUser2",
+        "ContributionStatus", "User1CreateAndMintContribution", "GuildContribution"
+      ];
+    for (const tableName of tableNamesOrActions){
+      cy.seedDB(tableName);
+    }
+    
+    cy.fixture('testaccounts.json').then((accounts) => {
+      this.accounts = accounts
+      //login User2
+      cy.login(this.accounts[0].address, this.accounts[0].privateKey);
+    });
+  
+    cy.get('[data-cy="myDashboards-btn"]', { timeout: 15000 })
+      .should('be.visible')
+      .click({ force: true });
+  
+    cy.visit('http://localhost:3000/#/contributions')
+  
+    cy.contains('Attestations')
+      .click({force:true})
+    
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(5000)
+     
+  });
+  
+  afterEach(() => {
+    //teardown 
+    cy.teardownDB(["Attestation", "GuildContribution", "Contribution", "ContributionStatus", "Guild",  "User"]);
+  });
+  
+  describe("Attestation flow", () => {
+    it("attest to a minted Contribution", ()=>{
+  
+      cy.get('input[title="Toggle Row Selected"]')
+        .click()
+  
+      cy.get('[data-testId="attest-testId"]')
+        .click()
+  
+      cy.contains('Attest to DAO Contributions')
+        .should('be.visible')
+  
+      cy.get('[data-testId="addAttestations-btn"]')
+        .click()
+  
+      cy.get(`.chakra-modal__close-btn`, {timeout:10000}) 
+        .click({force: true})
+  
+      // eslint-disable-next-line cypress/no-unnecessary-waiting 
+      cy.wait(5000) //wait for the attestation loading-process
+      cy.contains('My Attestations', {timeout:10000})
+        .should('be.visible')
+        .click({force:true})
+  
+      cy.contains(`These are Contributions that you have already Attested to.`, {timeout:10000})
+        .should('be.visible')
+  
+    });
+   
+  });
+
