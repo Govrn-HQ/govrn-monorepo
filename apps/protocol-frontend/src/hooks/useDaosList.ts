35a52605c7418f067db2c6a3c6034a79842ea419
Jonathan Prozzi <jonathanprozzi@gmail.com>
2022-10-03 12:04:14 -0400

    feature/split-dao-calls (PRO-433) (#217)
    * Split out dao calls with react-query

---
+++ apps/protocol-frontend/src/hooks/useDaosList.ts
@@ -0,0 +1,13 @@
+
+import { useUser } from '../contexts/UserContext';
+import { useQuery } from '@tanstack/react-query';
+import { UIGuilds } from '@govrn/ui-types';
+
+export const useDaosList = ({ ...args }) => {
+  const { govrnProtocol: govrn } = useUser()
+  const { isLoading, isFetching, isError, error, data } = useQuery(['userDaos', args], async () => {
+    const data = await govrn.guild.list({ ...args })
+    return data
+  })
+  return { isLoading, isError, isFetching, error, data };
+}
