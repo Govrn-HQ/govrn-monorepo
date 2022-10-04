5bc9986c7fd2cc143f6b1a48025b74f110d4de27
Nicholas Koech <nicholask320@gmail.com>
2022-10-04 15:20:20 -0700

    add types to index.ts

--- apps/protocol-frontend-e2e/src/support/index.ts
+++ apps/protocol-frontend-e2e/src/support/index.ts
@@ -15,3 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands';
+export type JSONPrimitive = string | number | boolean | null
+export type JSONArray = JSONValue[]
+export type JSONValue = JSONArray | JSONObject | JSONPrimitive
+export type JSONObject = { [k: string]: JSONValue }
