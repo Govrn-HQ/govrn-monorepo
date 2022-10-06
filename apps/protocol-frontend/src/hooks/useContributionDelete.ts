ed45b4fd8eb62a840d2cf7ff1e09b3ccaa8818e6
jonathanprozzi <jonathanprozzi@gmail.com>
2022-10-06 15:30:05 -0400

    removed Interface since only using single prop -- added type inline

--- apps/protocol-frontend/src/hooks/useContributionDelete.ts
+++ apps/protocol-frontend/src/hooks/useContributionDelete.ts
@@ -5,44 +5,17 @@ import { useOverlay } from '../contexts/OverlayContext';
 import { ContributionFormValues } from '../types/forms';
 import { UIContribution } from '@govrn/ui-types';
 
-interface DeleteContributionProps {
-  updatedValues: ContributionFormValues;
-  contribution: UIContribution;
-  bulkItemCount?: number;
-}
-
 export const useContributionDelete = () => {
   const toast = useToast()
   const { setModals } = useOverlay();
   const { govrnProtocol: govrn, userData } = useUser()
   const queryClient = useQueryClient()
   const toastUpdateContributionId = 'toast-update-contribution';
-  const { mutate, mutateAsync, isLoading, isError, isSuccess } = useMutation(async ({ updatedValues, contribution }: DeleteContributionProps) => {
-    if (userData !== null) {
-      const data = await govrn.custom.updateUserContribution({
-        address: userData.address,
-        chainName: 'ethereum',
-        userId: userData.id,
-        name: updatedValues.name ?? contribution.name,
-        details: updatedValues.details ?? contribution.details,
-        proof: updatedValues.proof ?? contribution.proof,
-        activityTypeName:
-          updatedValues.activityType ?? contribution.activity_type.name,
-        dateOfEngagement: new Date(
-          updatedValues.engagementDate ?? contribution.date_of_engagement,
-        ).toISOString(),
-        status: 'staging', // should always be staging since can only update staged Contributions
-        guildId: updatedValues.daoId === null ? null : Number(updatedValues.daoId),
-        contributionId: contribution.id,
-        currentGuildId: contribution.guilds[0]?.guild?.id || undefined,
-        contributionUserAddress: contribution.user?.address,
-      })
-      return data
-    }
+  const { mutate, mutateAsync, isLoading, isError, isSuccess } = useMutation(async (contributionId: number) => {
+    // delete the contribution!
   },
     {
-      onSuccess: (_, { bulkItemCount }) => { // destructure the bulkItemCount from the variables (args passed into the mutation)
-        console.log('firing success toast')
+      onSuccess: () => { // destructure the bulkItemCount from the variables (args passed into the mutation)
         queryClient.invalidateQueries(['activityTypes']) // invalidate the activity types query -- covers all args
         queryClient.invalidateQueries(['userDaos'])  // invalidate the userDaos query -- covers all args
         if (!toast.isActive(toastUpdateContributionId)) {
