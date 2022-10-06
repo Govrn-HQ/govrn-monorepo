4bebd474c2038a57c18fb10db8088233e2c61a6b
jonathanprozzi <jonathanprozzi@gmail.com>
2022-10-06 15:37:08 -0400

    added delete sdk call and args to the hook

--- apps/protocol-frontend/src/hooks/useContributionDelete.ts
+++ apps/protocol-frontend/src/hooks/useContributionDelete.ts
@@ -1,42 +1,44 @@
 import { useUser } from '../contexts/UserContext';
+import { useNetwork, useSigner } from 'wagmi';
 import { useToast } from '@chakra-ui/react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useOverlay } from '../contexts/OverlayContext';
-import { ContributionFormValues } from '../types/forms';
-import { UIContribution } from '@govrn/ui-types';
+import { networks } from '../utils/networks';
 
 export const useContributionDelete = () => {
   const toast = useToast()
   const { setModals } = useOverlay();
   const { govrnProtocol: govrn, userData } = useUser()
-  const queryClient = useQueryClient()
-  const toastUpdateContributionId = 'toast-update-contribution';
+  const { data: signer } = useSigner();
+  const { chain } = useNetwork();
   const { mutate, mutateAsync, isLoading, isError, isSuccess } = useMutation(async (contributionId: number) => {
-    // delete the contribution!
+    if (signer && chain?.id) {
+      await govrn.contribution.delete(
+        {
+          address: networks[chain?.id].govrnContract,
+          chainId: chain?.id,
+          name: networks[chain?.id].name,
+        },
+        signer,
+        contributionId,
+      );
+    }
   },
     {
-      onSuccess: () => { // destructure the bulkItemCount from the variables (args passed into the mutation)
-        queryClient.invalidateQueries(['activityTypes']) // invalidate the activity types query -- covers all args
-        queryClient.invalidateQueries(['userDaos'])  // invalidate the userDaos query -- covers all args
-        if (!toast.isActive(toastUpdateContributionId)) {
-          toast({
-            id: toastUpdateContributionId,
-            title: `Contribution ${bulkItemCount && bulkItemCount > 0 ? 'Reports' : 'Report'
-              } Updated`,
-            description: `Your Contribution ${bulkItemCount && bulkItemCount > 0 ? 'Reports have' : 'Report has'
-              } been updated.`,
-            status: 'success',
-            duration: 3000,
-            isClosable: true,
-            position: 'top-right',
-          });
-        }
-        setModals({ editContributionFormModal: false });
+      onSuccess: () => {
+        toast({
+          title: 'Contribution successfully deleted',
+          description: 'Your Contribution has been deleted.',
+          status: 'success',
+          duration: 3000,
+          isClosable: true,
+          position: 'top-right',
+        });
       },
       onError: (error) => {
         console.log('error', error)
         toast({
-          title: 'Unable to Update Contribution',
+          title: 'Unable to delete contribution',
           description: `Something went wrong. Please try again.`,
           status: 'error',
           duration: 3000,
