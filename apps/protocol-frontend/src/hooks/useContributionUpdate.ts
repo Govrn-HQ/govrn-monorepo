438fce1ccb922b7e4975541e9590cbe06ff74bff
Alex Keating <keating.dev@protonmail.com>
2022-10-07 12:16:56 -0400

    Add invalidation for contribution list

--- apps/protocol-frontend/src/hooks/useContributionUpdate.ts
+++ apps/protocol-frontend/src/hooks/useContributionUpdate.ts
@@ -12,45 +12,56 @@ interface UpdateContributionProps {
 }
 
 export const useContributionUpdate = () => {
-  const toast = useToast()
+  const toast = useToast();
   const { setModals } = useOverlay();
-  const { govrnProtocol: govrn, userData } = useUser()
-  const queryClient = useQueryClient()
+  const { govrnProtocol: govrn, userData } = useUser();
+  const queryClient = useQueryClient();
   const toastUpdateContributionId = 'toast-update-contribution';
-  const { mutate, mutateAsync, isLoading, isError, isSuccess } = useMutation(async ({ updatedValues, contribution, bulkItemCount }: UpdateContributionProps) => {
-    if (userData !== null) {
-      const data = await govrn.custom.updateUserContribution({
-        address: userData.address,
-        chainName: 'ethereum',
-        userId: userData.id,
-        name: updatedValues.name ?? contribution.name,
-        details: updatedValues.details ?? contribution.details,
-        proof: updatedValues.proof ?? contribution.proof,
-        activityTypeName:
-          updatedValues.activityType ?? contribution.activity_type.name,
-        dateOfEngagement: new Date(
-          updatedValues.engagementDate ?? contribution.date_of_engagement,
-        ).toISOString(),
-        status: 'staging', // should always be staging since can only update staged Contributions
-        guildId: updatedValues.daoId === null ? null : Number(updatedValues.daoId),
-        contributionId: contribution.id,
-        currentGuildId: contribution.guilds[0]?.guild?.id || undefined,
-        contributionUserAddress: contribution.user?.address,
-      })
-      return data
-    }
-  },
+  const { mutate, mutateAsync, isLoading, isError, isSuccess } = useMutation(
+    async ({
+      updatedValues,
+      contribution,
+      bulkItemCount,
+    }: UpdateContributionProps) => {
+      if (userData !== null) {
+        const data = await govrn.custom.updateUserContribution({
+          address: userData.address,
+          chainName: 'ethereum',
+          userId: userData.id,
+          name: updatedValues.name ?? contribution.name,
+          details: updatedValues.details ?? contribution.details,
+          proof: updatedValues.proof ?? contribution.proof,
+          activityTypeName:
+            updatedValues.activityType ?? contribution.activity_type.name,
+          dateOfEngagement: new Date(
+            updatedValues.engagementDate ?? contribution.date_of_engagement,
+          ).toISOString(),
+          status: 'staging', // should always be staging since can only update staged Contributions
+          guildId:
+            updatedValues.daoId === null ? null : Number(updatedValues.daoId),
+          contributionId: contribution.id,
+          currentGuildId: contribution.guilds[0]?.guild?.id || undefined,
+          contributionUserAddress: contribution.user?.address || undefined,
+        });
+        return data;
+      }
+    },
     {
-      onSuccess: (_, { bulkItemCount }) => { // destructure the bulkItemCount from the variables (args passed into the mutation)
-        queryClient.invalidateQueries(['activityTypes']) // invalidate the activity types query -- covers all args
-        queryClient.invalidateQueries(['userDaos'])  // invalidate the userDaos query -- covers all args
+      onSuccess: (_, { bulkItemCount }) => {
+        // destructure the bulkItemCount from the variables (args passed into the mutation)
+        queryClient.invalidateQueries(['activityTypes']); // invalidate the activity types query -- covers all args
+        queryClient.invalidateQueries(['userDaos']); // invalidate the userDaos query -- covers all args
+        queryClient.invalidateQueries(['contributionList']);
+        queryClient.invalidateQueries(['contributionInfiniteList']);
         if (!toast.isActive(toastUpdateContributionId)) {
           toast({
             id: toastUpdateContributionId,
-            title: `Contribution ${bulkItemCount && bulkItemCount > 0 ? 'Reports' : 'Report'
-              } Updated`,
-            description: `Your Contribution ${bulkItemCount && bulkItemCount > 0 ? 'Reports have' : 'Report has'
-              } been updated.`,
+            title: `Contribution ${
+              bulkItemCount && bulkItemCount > 0 ? 'Reports' : 'Report'
+            } Updated`,
+            description: `Your Contribution ${
+              bulkItemCount && bulkItemCount > 0 ? 'Reports have' : 'Report has'
+            } been updated.`,
             status: 'success',
             duration: 3000,
             isClosable: true,
@@ -59,8 +70,8 @@ export const useContributionUpdate = () => {
         }
         setModals({ editContributionFormModal: false });
       },
-      onError: (error) => {
-        console.log('error', error)
+      onError: error => {
+        console.log('error', error);
         toast({
           title: 'Unable to Update Contribution',
           description: `Something went wrong. Please try again.`,
@@ -70,7 +81,7 @@ export const useContributionUpdate = () => {
           position: 'top-right',
         });
       },
-    }
-  )
-  return { mutate, mutateAsync, isLoading, isError, isSuccess }
-}
+    },
+  );
+  return { mutate, mutateAsync, isLoading, isError, isSuccess };
+};
