62b757a2c61c53fffda48347aadd2c7840e99fc0
Amr Elghobary <amr.elghobary@gmail.com>
2022-10-20 20:26:07 +0200

    Handle parsing error in fetching ipfs

--- apps/contract-sync-job/src/utils.ts
+++ apps/contract-sync-job/src/utils.ts
@@ -6,11 +6,20 @@ const getIpfsFromHash = (hash: string) => {
   return `${INFURA_SUBDOMAIN}/api/v0/cat?arg=${hash.split('/').slice(2)}`;
 };
 
-export const fetchIPFS = async <T>(ipfsHash: string): Promise<T> => {
+export const fetchIPFS = async <T extends Record<string, any>>(
+  ipfsHash: string,
+): Promise<T> => {
   const resp = await fetch(getIpfsFromHash(ipfsHash), {
     method: 'post',
   });
-  return JSON.parse(await (await resp?.blob())?.text()) as T;
+  const text = await (await resp?.blob())?.text();
+
+  try {
+    return JSON.parse(text) as T;
+  } catch (error) {
+    console.error(':: Failed to parse:', text);
+    throw error;
+  }
 };
 
 export const logger = (msg: string, ...optionalParams: unknown[]) => {
