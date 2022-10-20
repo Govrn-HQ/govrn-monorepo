9fd947b94c84abc013957bfb823deec26d62f1af
Jonathan Prozzi <jonathanprozzi@gmail.com>
2022-10-20 15:22:11 -0400

    feature/dao-dashboard-auth-wrapper (PRO-526) (#255)
    * Add initial dao dashboard page

--- apps/protocol-frontend/src/Routes.tsx
+++ apps/protocol-frontend/src/Routes.tsx
@@ -4,17 +4,21 @@ import {
   Routes as RouteContainer,
   Navigate,
   useLocation,
+  useParams,
 } from 'react-router-dom';
 import { useAuth } from './contexts/AuthContext';
+import { useUser } from './contexts/UserContext';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Contributions from './pages/Contributions';
 import Attestations from './pages/Attestations';
 import Dashboard from './pages/Dashboard';
+import DaoDashboard from './pages/DaoDashboard';
 import Report from './pages/Report';
 import FourOFour from './pages/404';
 import RedirectHome from './pages/Redirect';
 import ContributionDetail from './pages/ContributionDetail';
+import ErrorView from './components/ErrorView';
 
 const RequireActiveUser = ({ children }: { children: JSX.Element }) => {
   const location = useLocation();
@@ -24,6 +28,22 @@ const RequireActiveUser = ({ children }: { children: JSX.Element }) => {
     return <Navigate to="/authenticate" state={{ from: location }} replace />;
   }
 
+  return children;
+};
+
+const RequireDaoUser = ({ children }: { children: JSX.Element }) => {
+  const { isUserDaoMember } = useUser();
+  const { guildId } = useParams();
+  if (guildId) {
+    if (isUserDaoMember(guildId) === false) {
+      return (
+        <ErrorView
+          errorMessage="You have to be a member of this DAO to view the DAO Dashboard."
+          includeMotto={false}
+        />
+      );
+    }
+  }
   return children;
 };
 
@@ -71,6 +91,16 @@ const Routes = () => {
           element={
             <RequireActiveUser>
               <Dashboard />
+            </RequireActiveUser>
+          }
+        />
+        <Route
+          path="/feature/dao/:guildId"
+          element={
+            <RequireActiveUser>
+              <RequireDaoUser>
+                <DaoDashboard />
+              </RequireDaoUser>
             </RequireActiveUser>
           }
         />
