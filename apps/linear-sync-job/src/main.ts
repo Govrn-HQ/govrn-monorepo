d9499d16b6a99b0e6bf4ec2daa849404ad319c71
Alex Keating <keating.dev@protonmail.com>
2022-10-22 10:45:48 -0400

    Reorganize job

--- apps/linear-sync-job/src/main.ts
+++ apps/linear-sync-job/src/main.ts
@@ -1,94 +1,137 @@
-import { LinearClient, User, Cycle, Project, Team } from '@linear/sdk';
+import {
+  LinearClient,
+  IssueConnection,
+  Issue,
+  LinearDocument,
+} from '@linear/sdk';
 import {
   GovrnProtocol,
   SortOrder,
-  ContributionCreateManyInput,
   LinearIssueCreateManyInput,
+  UpsertActivityTypeMutation,
+  GetContributionStatusQuery,
+  ListLinearUsersQuery,
 } from '@govrn/protocol-client';
 import 'tslib';
 
-const apiKey = process.env.API_KEY;
 const protcolUrl = process.env.PROTOCOL_URL;
 const protocolApiToken = process.env.PROTOCOL_API_TOKEN;
-const jobName = 'linear-sync-job';
 
-function sleep(ms) {
-  return new Promise(resolve => {
-    setTimeout(resolve, ms);
-  });
-}
+const STORE_WORKER_COUNT = 4;
+const MAIN_WORKER_COUNT = 4;
 
-const upsertUser = (govrn: GovrnProtocol, user: User) => {
-  const linearAssignee = {
-    active: user.active,
-    displayName: user.displayName,
-    email: user.email,
-    linear_id: user.id,
-    name: user.name,
-    url: user.url,
-  };
-  const assingneePromise = govrn.linear.user.upsert({
-    create: linearAssignee,
-    update: { name: { set: user.name } },
-    where: { linear_id: user.id },
-  });
-  return assingneePromise;
-};
-
-const upsertCycle = (govrn: GovrnProtocol, cycle: Cycle) => {
-  const linearCycle = {
-    endsAt: cycle.endsAt,
-    linear_id: cycle.id,
-    number: cycle.number,
-    startsAt: cycle.startsAt,
-  };
-  const cyclePromise = govrn.linear.cycle.upsert({
-    create: linearCycle,
-    update: { linear_id: { set: cycle.id } },
-    where: { linear_id: cycle.id },
-  });
-  return cyclePromise;
-};
-
-const upsertProject = (govrn: GovrnProtocol, project: Project) => {
-  const linearProject = {
-    linear_id: project.id,
-    name: project.name,
-  };
-  const projectPromise = govrn.linear.project.upsert({
-    create: linearProject,
-    update: { linear_id: { set: project.id } },
-    where: { linear_id: project.id },
-  });
-  return projectPromise;
-};
-
-const upsertTeam = (govrn: GovrnProtocol, team: Team) => {
-  const linearTeam = {
-    linear_id: team.id,
-    name: team.name,
-    key: team.key,
-  };
-  const teamPromise = govrn.linear.team.upsert({
-    create: linearTeam,
-    update: { linear_id: { set: team.id } },
-    where: { linear_id: team.id },
-  });
-  return teamPromise;
-};
-
-const createJobRun = async (
-  govrn: GovrnProtocol,
-  job: { startDate: Date; completedDate: Date },
+const fetchLinearIssues = async (
+  linearClient: LinearClient,
+  completedAtFilter: { gt: Date },
 ) => {
-  const teamPromise = await govrn.jobRun.create({
-    data: {
-      startDate: job.startDate,
-      completedDate: job.completedDate,
-      name: jobName,
-    },
+  try {
+    return await linearClient.issues({
+      filter: {
+        and: [{ completedAt: completedAtFilter }],
+      },
+      orderBy: LinearDocument.PaginationOrderBy.CreatedAt,
+      first: 100,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// sort low to high
+const processIssue = async (
+  issues: Issue[],
+  resp: IssueConnection,
+  govrn: GovrnProtocol,
+  activity: UpsertActivityTypeMutation['upsertActivityType'],
+  contributionStatus: GetContributionStatusQuery['contributionStatuses'][0],
+  user: ListLinearUsersQuery['result'][0],
+) => {
+  let page = 1;
+  let storePromises = [];
+  console.log(`Processing first issues for user ${user.id}`);
+  storePromises.push(
+    storeContributions(issues, govrn, activity, contributionStatus, user),
+  );
+  while (issues.length > 0) {
+    try {
+      const next = await resp.fetchNext();
+      console.log(`Processing ${page * 100 + 1} for user ${user.id}`);
+      issues = next.nodes.slice(page * 100 + 1);
+      const p = storeContributions(
+        issues,
+        govrn,
+        activity,
+        contributionStatus,
+        user,
+      );
+      storePromises.push(p);
+      if (storePromises.length >= STORE_WORKER_COUNT) {
+        await Promise.all(storePromises);
+        storePromises = [];
+      }
+      page = page + 1;
+    } catch (e) {
+      console.error(e);
+      break;
+    }
+  }
+  await Promise.all(storePromises);
+};
+
+const storeContributions = async (
+  issues: Issue[],
+  govrn: GovrnProtocol,
+  activity: UpsertActivityTypeMutation['upsertActivityType'],
+  contributionStatus: GetContributionStatusQuery['contributionStatuses'][0],
+  user: ListLinearUsersQuery['result'][0],
+) => {
+  const linearIssues = [] as LinearIssueCreateManyInput[];
+  for (const issue of issues) {
+    const contribution = await govrn.contribution.create({
+      data: {
+        // activity type should be linear
+        activity_type: { connect: { id: activity.id } },
+        date_of_engagement: issue.completedAt,
+        details: issue.description,
+        name: issue.title,
+        status: { connect: { id: contributionStatus?.id } },
+        proof: issue.url,
+        user: { connect: { id: user.id } },
+      },
+    });
+
+    linearIssues.push({
+      archivedAt: issue.archivedAt,
+      autoArchivedAt: issue.autoArchivedAt,
+      autoClosedAt: issue.autoClosedAt,
+      boardOrder: issue.boardOrder,
+      branchName: issue.branchName,
+      canceledAt: issue.canceledAt,
+      completedAt: issue.completedAt,
+      createdAt: issue.createdAt,
+      customerTickerCount: issue.customerTicketCount,
+      description: issue.description,
+      dueDate: issue.dueDate,
+      estimate: issue.estimate,
+      identifier: issue.identifier,
+      linear_id: issue.id,
+      priority: issue.priority,
+      priorityLabel: issue.priorityLabel,
+      snoozedUntilAt: issue.snoozedUntilAt,
+      sortOrder: issue.sortOrder,
+      startedAt: issue.startedAt,
+      subIssueSortOrder: issue.subIssueSortOrder,
+      title: issue.title,
+      trashed: issue.trashed || false,
+      updatedAt: issue.updatedAt,
+      url: issue.url,
+      contribution_id: contribution.id,
+    });
+  }
+  await govrn.linear.issue.bulkCreate({
+    data: linearIssues,
+    skipDuplicates: true,
   });
-  return teamPromise;
 };
 
 const main = async () => {
@@ -107,13 +150,13 @@ const main = async () => {
     where: { name: 'Linear Issue' },
   });
   for await (const result of users) {
+    let wp = [];
     for (const user of result.result) {
+      console.log(`Proccessing linear for user ${user.id}`);
       const linearClient = new LinearClient({ apiKey: user.access_token });
-      let issues = [];
-      let page = 1;
       const lastIssue = await govrn.linear.issue.list({
         first: 1,
-        orderBy: [{ linear_id: SortOrder.Desc }],
+        orderBy: [{ completedAt: SortOrder.Desc }],
       });
       const contributionStatus = await govrn.contribution.status.get('staging');
 
@@ -121,140 +164,26 @@ const main = async () => {
         lastIssue.result.length > 0
           ? { gt: new Date(lastIssue.result[0].completedAt) }
           : { gt: new Date('1990-01-01T10:20:30Z') };
-      do {
-        let resp;
-        try {
-          resp = await linearClient.issues({
-            filter: {
-              and: [{ completedAt: completedAtFilter }],
-            },
-            first: 100,
-          });
-        } catch (e) {
-          console.error(e);
-          await sleep(1000 * 60 * 60);
-          resp = await linearClient.issues({
-            filter: {
-              and: [{ completedAt: completedAtFilter }],
-            },
-            first: 100,
-          });
-        }
-        issues = resp.nodes;
-        console.log(`Processing ${issues.length} issues`);
-        const linearIssues = [] as LinearIssueCreateManyInput[];
-        for (const issue of issues) {
-          console.log(`Processing ${issue.title}`);
-          let issueAssignee;
-          let issueCreator;
-          let issueCycle;
-          let issueProject;
-          let issueTeam;
-          try {
-            [issueAssignee, issueCreator, issueCycle, issueProject, issueTeam] =
-              await Promise.all([
-                issue.assignee,
-                issue.creator,
-                issue.cycle,
-                issue.project,
-                issue.team,
-              ]);
-          } catch (e) {
-            console.error(e);
-            await sleep(1000 * 60 * 60);
-            [issueAssignee, issueCreator, issueCycle, issueProject, issueTeam] =
-              await Promise.all([
-                issue.assignee,
-                issue.creator,
-                issue.cycle,
-                issue.project,
-                issue.team,
-              ]);
-          }
 
-          let assingneePromise;
-          if (issueAssignee) {
-            assingneePromise = await upsertUser(govrn, issueAssignee);
-          }
-          let creatorPromise;
-          if (issueCreator) {
-            creatorPromise = await upsertUser(govrn, issueCreator);
-          }
-          let cyclePromise;
-          if (issueCycle) {
-            cyclePromise = upsertCycle(govrn, issueCycle);
-          }
-          let projectPromise;
-          if (issueProject) {
-            projectPromise = upsertProject(govrn, issueProject);
-          }
-          const teamPromise = upsertTeam(govrn, issueTeam);
-
-          const [assignee, creator, cycle, project, team] = await Promise.all([
-            assingneePromise,
-            creatorPromise,
-            cyclePromise,
-            projectPromise,
-            teamPromise,
-          ]);
-          const contribution = await govrn.contribution.create({
-            data: {
-              // activity type should be linear
-              activity_type: { connect: { id: activity.id } },
-              date_of_engagement: issue.completedAt,
-              details: issue.description,
-              name: issue.title,
-              status: { connect: { id: contributionStatus?.id } },
-              proof: issue.url,
-              user: { connect: { id: user.id } },
-            },
-          });
-
-          linearIssues.push({
-            archivedAt: issue.archivedAt,
-            assignee_id: assignee?.id,
-            autoArchivedAt: issue.autoArchivedAt,
-            autoClosedAt: issue.autoClosedAt,
-            boardOrder: issue.boardOrder,
-            branchName: issue.branchName,
-            canceledAt: issue.canceledAt,
-            completedAt: issue.completedAt,
-            createdAt: issue.createdAt,
-            creator_id: creator?.id,
-            customerTickerCount: issue.customerTicketCount,
-            cycle_id: cycle?.id,
-            description: issue.description,
-            dueDate: issue.dueDate,
-            estimate: issue.estimate,
-            identifier: issue.identifier,
-            linear_id: issue.id,
-            priority: issue.priority,
-            priorityLabel: issue.priorityLabel,
-            project_id: project?.id,
-            snoozedUntilAt: issue.snoozedUntilAt,
-            sortOrder: issue.sortOrder,
-            startedAt: issue.startedAt,
-            subIssueSortOrder: issue.subIssueSortOrder,
-            team_id: team.id,
-            title: issue.title,
-            trashed: issue.trashed || false,
-            updatedAt: issue.updatedAt,
-            url: issue.url,
-            contribution_id: contribution.id,
-          });
-        }
-        await govrn.linear.issue.bulkCreate({
-          data: linearIssues,
-          skipDuplicates: true,
-        });
-        if (linearIssues.length < 100) {
-          break;
-        }
-        const next = await resp.fetchNext();
-        issues = next.nodes.slice(page * 100 + 1);
-        page = page + 1;
-      } while (issues.length > 0);
+      const resp = await fetchLinearIssues(linearClient, completedAtFilter);
+      if (resp.nodes.length == 0) {
+        continue;
+      }
+      const workPromise = processIssue(
+        resp.nodes,
+        resp,
+        govrn,
+        activity,
+        contributionStatus,
+        user,
+      );
+      wp.push(workPromise);
+      if (wp.length > MAIN_WORKER_COUNT) {
+        await Promise.all(wp);
+        wp = [];
+      }
     }
+    await Promise.all(wp);
   }
   console.log(`Finished processing linear issues`);
 };
