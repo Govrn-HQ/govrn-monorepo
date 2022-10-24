7925cfab22095a3798d0bb216021edd1f42be63f
Amr Elghobary <amr.elghobary@gmail.com>
2022-10-24 22:47:57 +0200

    Fix import lint

--- libs/protocol-client/src/lib/client/chain.ts
+++ libs/protocol-client/src/lib/client/chain.ts
@@ -1,5 +1,5 @@
 import { BaseClient } from './base';
-import { GetChainQueryVariables } from '@govrn/protocol-client';
+import { GetChainQueryVariables } from '../protocol-types';
 
 export class Chain extends BaseClient {
   public async get(args: GetChainQueryVariables) {
