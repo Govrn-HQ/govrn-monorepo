6b2c46eea82d323d1c81460eab83adb11a1bf4c2
jonathanprozzi <jonathanprozzi@gmail.com>
2022-11-03 13:39:32 -0400

    better layout for mobile -- trimmed space in UI in desktop as well

--- apps/protocol-frontend/src/components/ContributionsByDateChart.tsx
+++ apps/protocol-frontend/src/components/ContributionsByDateChart.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Flex, Box, Heading, Text, useBreakpointValue } from '@chakra-ui/react';
-import { ResponsiveTimeRange, CalendarTooltipProps } from '@nivo/calendar';
+import {
+  TimeRange,
+  ResponsiveTimeRange,
+  CalendarTooltipProps,
+} from '@nivo/calendar';
 import { GovrnTheme, GovrnSpinner } from '@govrn/protocol-ui';
 import { TODAY_DATE, YEAR, BRAND_COLOR_MAP } from '../utils/constants';
 import { endOfDay, subWeeks } from 'date-fns';
@@ -87,11 +91,7 @@ const ContributionsByDateChart = ({
     <Flex direction="column" paddingBottom={4} paddingX={{ base: 4, lg: 0 }}>
       <Flex
         direction="column"
-        minHeight={{ base: '5rem', lg: '15rem' }}
-        height={{ base: '5rem', lg: '15rem' }}
-        width="100%"
-        maxWidth="100vw"
-        paddingY={{ base: '0', lg: '4' }}
+        paddingY={{ base: '4', lg: '4' }}
         paddingX={{ base: '0', lg: '4' }}
         color="gray.700"
         background="white"
@@ -109,43 +109,52 @@ const ContributionsByDateChart = ({
           Contributions By Date
         </Heading>
         {contributionsCountMap?.length !== 0 && (
-          <ResponsiveTimeRange
-            data={
-              contributionsCountMap
-                ? contributionsCountMap.filter(
-                    contribution => contribution.value !== 0,
-                  )
-                : []
-            }
-            from={subWeeks(new Date(), YEAR)}
-            to={endOfDay(TODAY_DATE)}
-            weekdayTicks={isMobile ? [] : [1, 3, 5]}
-            emptyColor="#eeeeee"
-            colors={[
-              brandColors[100],
-              brandColors[200],
-              brandColors[300],
-              brandColors[400],
-              brandColors[500],
-              brandColors[600],
-              brandColors[700],
-            ]}
-            dayRadius={1}
-            margin={
-              isMobile
-                ? { top: 20, right: 10, bottom: 0, left: 10 }
-                : { top: 20, right: 20, bottom: 20, left: 20 }
-            }
-            dayBorderWidth={2}
-            dayBorderColor="#ffffff"
-            tooltip={CustomTooltipDailyContributions}
-          />
+          <Box
+            alignSelf="flex-start"
+            height={{ base: '5rem', lg: '10rem' }}
+            width="100%"
+            paddingX={{ base: 2, lg: 0 }}
+          >
+            <ResponsiveTimeRange
+              data={
+                contributionsCountMap
+                  ? contributionsCountMap.filter(
+                      contribution => contribution.value !== 0,
+                    )
+                  : []
+              }
+              from={subWeeks(new Date(), YEAR)}
+              to={endOfDay(TODAY_DATE)}
+              emptyColor="#eeeeee"
+              weekdayTicks={isMobile ? [] : [1, 3, 5]}
+              weekdayLegendOffset={isMobile ? -5 : 75} // forcing this to hide the days -- it renders as part of the dom in the svg even with an empty array
+              colors={[
+                brandColors[100],
+                brandColors[200],
+                brandColors[300],
+                brandColors[400],
+                brandColors[500],
+                brandColors[600],
+                brandColors[700],
+              ]}
+              dayRadius={1}
+              margin={
+                isMobile
+                  ? { top: 20, right: 10, bottom: 0, left: 10 }
+                  : { top: 20, right: 20, bottom: 20, left: 20 }
+              }
+              dayBorderWidth={2}
+              dayBorderColor="#ffffff"
+              tooltip={CustomTooltipDailyContributions}
+            />
+          </Box>
         )}
 
         <Flex
           direction="row"
           alignItems="center"
-          justifyContent={{ base: 'flex-start', lg: 'center' }}
+          justifyContent={{ base: 'center', lg: 'center' }}
+          paddingX={{ base: 2, lg: 0 }}
         >
           <Text as="span" fontSize="sm" fontWeight="normal" paddingRight={1}>
             Less
