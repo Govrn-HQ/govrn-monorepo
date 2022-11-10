da45e08f1665b07522d0a9e5924ace17077a7c50
jonathanprozzi <jonathanprozzi@gmail.com>
2022-11-10 01:16:19 -0500

    added basic empty state

--- apps/protocol-frontend/src/pages/DaoLandingPage.tsx
+++ apps/protocol-frontend/src/pages/DaoLandingPage.tsx
@@ -1,6 +1,6 @@
 import { useAccount } from 'wagmi';
 import { useAuth } from '../contexts/AuthContext';
-import { Box } from '@chakra-ui/react';
+import { Box, Container, Stack, Text } from '@chakra-ui/react';
 import SiteLayout from '../components/SiteLayout';
 
 const DaoLandingPage = () => {
@@ -8,7 +8,37 @@ const DaoLandingPage = () => {
   const { isAuthenticated } = useAuth();
 
   return (
-    <SiteLayout>{isConnected && isAuthenticated && <Box>Test</Box>}</SiteLayout>
+    <SiteLayout>
+      {isConnected && isAuthenticated && (
+        <Container
+          paddingY={{ base: '4', md: '8' }}
+          paddingX={{ base: '0', md: '8' }}
+          color="gray.700"
+          maxWidth="1200px"
+        >
+          <Box
+            background="white"
+            boxShadow="sm"
+            borderRadius={{ base: 'none', md: 'lg' }}
+          >
+            <Stack spacing="4" justify="center" align="center" minHeight="50vh">
+              <Text fontSize="lg" fontWeight="medium">
+                <span aria-labelledby="Sun emoji for callout indicating user is not in any DAOs yet.">
+                  🌞
+                </span>{' '}
+                You're not in any DAOs yet!{' '}
+                <span
+                  role="img"
+                  aria-labelledby="Sun emoji for callout indicating user is not in any DAOs yet."
+                >
+                  🌞
+                </span>{' '}
+              </Text>
+            </Stack>
+          </Box>
+        </Container>
+      )}
+    </SiteLayout>
   );
 };
 
