9c3539779fdf5dbcc2b2ca7a995fa34299d262c9
Amr Elghobary <amr.elghobary@proton.me>
2022-11-17 14:02:37 +0200

    Fix prettier

--- apps/protocol-frontend/src/pages/Home.tsx
+++ apps/protocol-frontend/src/pages/Home.tsx
@@ -6,7 +6,6 @@ import HomeShell from '../components/HomeShell';
 // here
 const Home = () => {
   return (
-
     <Box as="section" height="100vh" bg="gray.50" overflowY="auto">
       <HomeShell />
     </Box>
