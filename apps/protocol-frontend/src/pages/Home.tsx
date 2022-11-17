b1a285709ca8c77d5cf347b755b4759666aa4255
Amr Elghobary <amr.elghobary@proton.me>
2022-11-17 13:54:19 +0200

    commit with prettier fail

--- apps/protocol-frontend/src/pages/Home.tsx
+++ apps/protocol-frontend/src/pages/Home.tsx
@@ -6,6 +6,7 @@ import HomeShell from '../components/HomeShell';
 // here
 const Home = () => {
   return (
+
     <Box as="section" height="100vh" bg="gray.50" overflowY="auto">
       <HomeShell />
     </Box>
