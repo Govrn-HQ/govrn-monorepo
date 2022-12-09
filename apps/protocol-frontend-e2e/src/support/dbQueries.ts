c9f32c851437f10b1dd737f2d8b4e6c4eb2562e5
Amr Elghobary <amr.elghobary@proton.me>
2022-12-09 18:07:26 +0200

    Fixing cypress tests

--- apps/protocol-frontend-e2e/src/support/dbQueries.ts
+++ apps/protocol-frontend-e2e/src/support/dbQueries.ts
@@ -8,12 +8,11 @@ import {
 
 export const create_user = (userData: JSONObject) => {
   const query = `
-    INSERT INTO "User" (name, address, chain_type_id, active, email )
-    VALUES ('${userData.username}', '${userData.address}', ${userData.chain_type_id}
-      , ${userData.active}, '${userData.email}'
-    )
+    INSERT INTO "User" (name, address, chain_type_id, active, email)
+    VALUES ( '${userData.username}', '${userData.address}', ${userData.chain_type_id}
+           , ${userData.active}, '${userData.email}')
     ON CONFLICT DO NOTHING;
-    `;
+  `;
   return queryDB(query);
 };
 
@@ -22,7 +21,7 @@ export const create_chainType = (name: string) => {
     INSERT INTO "ChainType" (name)
     VALUES ('${name}')
     ON CONFLICT DO NOTHING;
-    `;
+  `;
   return queryDB(query);
 };
 
@@ -31,7 +30,7 @@ export const create_guild = (guild_name: string) => {
     INSERT INTO "Guild" (name)
     VALUES ('${guild_name}')
     ON CONFLICT DO NOTHING;
-    `;
+  `;
   return queryDB(query);
 };
 
@@ -39,10 +38,10 @@ export const create_contribution = (contributionData: JSONObject) => {
   const statusID = 1;
   const query = `
     INSERT INTO "Contribution" (name, status_id, activity_type_id, user_id, date_of_engagement, details, proof)
-        VALUES ('${contributionData.name}',${statusID} , ${contributionData.activityTypeID}, ${contributionData.userID},
-        current_timestamp, '${contributionData.details}', '${contributionData.proof}')
+    VALUES ('${contributionData.name}', ${statusID}, ${contributionData.activityTypeID}, ${contributionData.userID},
+            current_timestamp, '${contributionData.details}', '${contributionData.proof}')
     ON CONFLICT DO NOTHING;
-    `;
+  `;
   return queryDB(query);
 };
 
@@ -52,7 +51,7 @@ export const create_GuildContribution = (
   const query = `
     INSERT INTO "GuildContribution" (guild_id, contribution_id)
     VALUES (${ObjectData.guildID}, ${ObjectData.contributionID});
-    `;
+  `;
   return queryDB(query);
 };
 
@@ -69,90 +68,109 @@ export const create_MintedContribution = (contributionData: JSONObject) => {
 
 export const create_GuildUser = (GuildUserObject: GuildUserObjectType) => {
   const query = `
-    INSERT INTO "GuildUser" (user_id, guild_id)
-    VALUES (${GuildUserObject.userID}, ${GuildUserObject.guildID});
-    `;
+    INSERT INTO "GuildUser" (user_id, guild_id, membership_status_id)
+    VALUES (${GuildUserObject.userID}, ${GuildUserObject.guildID}, ${GuildUserObject.membershipID});
+  `;
   return queryDB(query);
 };
 
 export const delete_GuildUser = (guilID: number) => {
   const query = `
-    DELETE FROM "GuildUser"
-    CASCADE
+    DELETE
+    FROM "GuildUser" CASCADE
     WHERE guild_id = ${guilID};
-    `;
+  `;
   return queryDB(query);
 };
 
 export const delete_guild = (guild_name: string) => {
   const query = `
-    DELETE FROM "Guild"
-    CASCADE
+    DELETE
+    FROM "Guild" CASCADE
     WHERE name = '${guild_name}';
-    `;
+  `;
   return queryDB(query);
 };
 
 export const delete_contribution = (name: string) => {
   const query = `
-    DELETE FROM "Contribution"
-    CASCADE
+    DELETE
+    FROM "Contribution" CASCADE
     WHERE name = '${name}';
-    `;
+  `;
   return queryDB(query);
 };
 
 export const delete_attestation = (userID: number) => {
   const query = `
-    DELETE FROM "Attestation"
-    CASCADE
+    DELETE
+    FROM "Attestation" CASCADE
     WHERE user_id = '${userID}';
-    `;
+  `;
   return queryDB(query);
 };
 
 export const delete_GuildContribution = (guildID: number) => {
   const query = `
-    DELETE FROM "GuildContribution"
-    CASCADE
+    DELETE
+    FROM "GuildContribution" CASCADE
     WHERE guild_id = '${guildID}';
-    `;
+  `;
+  return queryDB(query);
+};
+
+export const delete_MembershipStatus = () => {
+  const query = `DELETE
+                 FROM "GuildMembershipStatus";`;
   return queryDB(query);
 };
 
 export const delete_user = (username: string) => {
   const query = `
-    DELETE FROM "User"
-    CASCADE
+    DELETE
+    FROM "User" CASCADE
     WHERE name = '${username}';
-    `;
+  `;
   return queryDB(query);
 };
 
 export const delete_chainType = (name: string) => {
   const query = `
-    DELETE FROM "ChainType"
-    CASCADE
+    DELETE
+    FROM "ChainType" CASCADE
     WHERE name = '${name}';
-    `;
+  `;
   return queryDB(query);
 };
 
 export const delete_UserActivity = (userID: string) => {
   const query = `
-    DELETE FROM "UserActivity"
-    CASCADE
+    DELETE
+    FROM "UserActivity" CASCADE
     WHERE user_id = '${userID}';
-    `;
+  `;
   return queryDB(query);
 };
 
 export const contribution_status = () => {
   const query = `
-    INSERT INTO "ContributionStatus" (name) VALUES
-      ('staging'),
-      ('minted')
+    INSERT INTO "ContributionStatus" (name)
+    VALUES ('staging'),
+           ('minted')
     ON CONFLICT DO NOTHING;
-    `;
+  `;
+  return queryDB(query);
+};
+
+export const create_MembershipStatus = () => {
+  const query = `
+    INSERT INTO "GuildMembershipStatus" (name)
+    VALUES ('Member'),
+           ('Contributor'),
+           ('Recruit'),
+           ('Supporter'),
+           ('Lurker')
+    ON CONFLICT DO NOTHING;
+  `;
   return queryDB(query);
 };
