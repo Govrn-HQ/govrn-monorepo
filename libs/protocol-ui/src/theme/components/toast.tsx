ec810146957e5774a41b58aa229afa6220527492
jonathanprozzi <jonathanprozzi@gmail.com>
2022-12-16 13:12:52 -0500

    testing to see if this can be done via a variant

---
+++ libs/protocol-ui/src/theme/components/toast.tsx
@@ -0,0 +1,12 @@
+export default {
+  variants: {
+    success: {
+      backgroundColor: 'brand.purple.100',
+      color: 'brand.purple',
+    },
+    error: {
+      backgroundColor: 'blue.100',
+      color: 'blue.500',
+    },
+  },
+};
