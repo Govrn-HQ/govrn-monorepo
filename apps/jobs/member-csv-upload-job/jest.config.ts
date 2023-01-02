100e82c79fd0242ac93b6909dd5cc518bdd2e9ca
alexkeating <keating.dev@protonmail.com>
2023-01-02 12:13:19 -0500

    Generate job

---
+++ apps/jobs/member-csv-upload-job/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'jobs-member-csv-upload-job',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/apps/jobs/member-csv-upload-job',
+};
