19933a52fb93089caa28b1bbfe52c514ac59eef2
jonathanprozzi <jonathanprozzi@gmail.com>
2023-01-05 15:40:30 -0500

    added a check to use the incoming value only if its in the users list (without relying on them touching the input)

--- apps/protocol-frontend/src/components/ReportForm.tsx
+++ apps/protocol-frontend/src/components/ReportForm.tsx
@@ -126,10 +126,6 @@ const ReportForm = ({ onFinish }: { onFinish: () => void }) => {
 
   const [isUserCreatingMore, setUserCreatingMore] = useState(false);
 
-  useEffect(() => {
-    setValue('engagementDate', engagementDateValue);
-  }, []);
-
   const {
     mutateAsync: createNewContribution,
     isLoading: createNewContributionIsLoading,
@@ -138,6 +134,7 @@ const ReportForm = ({ onFinish }: { onFinish: () => void }) => {
   const createContributionHandler: SubmitHandler<
     ContributionFormValues
   > = async values => {
+    console.log('values', values);
     if (selectedFile && fileError === null && ipfsError === false) {
       try {
         await uploadFileIpfs(selectedFile, false);
@@ -192,6 +189,13 @@ const ReportForm = ({ onFinish }: { onFinish: () => void }) => {
       value: dao.id,
       label: dao.name ?? '',
     })) || [];
+
+  useEffect(() => {
+    const matchedDao = daoListOptions.find(dao => dao.value === daoIdParam);
+    console.log('matched dao', matchedDao);
+    setValue('engagementDate', engagementDateValue);
+    setValue('daoId', matchedDao?.value ?? null); // allows user to submit contribution with a preset daoId query param without needing to touch the field
+  }, []);
 
   // there is an error with the query:
   if (userActivityTypesIsError) {
