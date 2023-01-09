ec46dd300bd9c4ffc4ddb8c0263c2f236cedf965
Amr Elghobary <amr.elghobary@proton.me>
2023-01-09 16:22:31 +0200

    Rename Job `guild-xyz` -> `guild-import-job`

--- apps/guild-import-job/jest.config.ts
+++ apps/guild-import-job/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'guild-xyz',
+  displayName: 'guild-import-job',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/guild-xyz',
+  coverageDirectory: '../../coverage/apps/guild-import-job',
 };
