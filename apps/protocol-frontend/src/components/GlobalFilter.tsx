2aa8a0f354ee2caa2265159d5a1e798ab044ea4c
Amr Elghobary <amr.elghobary@proton.me>
2023-01-25 18:17:52 +0200

    Remove `react-table` v7 deps

--- apps/protocol-frontend/src/components/GlobalFilter.tsx
+++ apps/protocol-frontend/src/components/GlobalFilter.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { useAsyncDebounce } from 'react-table';
 import {
   Flex,
   Icon,
@@ -8,6 +7,7 @@ import {
   InputLeftElement,
 } from '@chakra-ui/react';
 import { FiSearch } from 'react-icons/fi';
+import { DEFAULT_DELAY, useDebounce } from '../utils/useDebounce';
 
 interface GlobalFilterTypes<T> {
   preGlobalFilteredRows: T[];
@@ -25,9 +25,7 @@ const GlobalFilter = <T,>({
 
   const rowCount = preGlobalFilteredRows.length;
   const [value, setValue] = useState(globalFilter);
-  const onChangeDebouncer = useAsyncDebounce(value => {
-    setGlobalFilter(value || undefined);
-  }, 200);
+  const onChangeDebounce = useDebounce(setGlobalFilter, DEFAULT_DELAY);
 
   return (
     <Flex paddingX={{ base: '4', md: '6' }} paddingBottom={4}>
@@ -42,7 +40,7 @@ const GlobalFilter = <T,>({
           value={value || ''}
           onChange={e => {
             setValue(e.target.value);
-            onChangeDebouncer(e.target.value);
+            onChangeDebounce(e.target.value);
           }}
           variant="outline"
         />
---
+++ apps/protocol-frontend/src/utils/useDebounce.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+
+export const DEFAULT_DELAY = 5_00;
+
+/**
+ * This hooks applies given `fn` to passed values after `delay` time,
+ * allowing to debounce fast changing values
+ *
+ * @returns A callback to pass the fast-changing values.
+ */
+export const useDebounce = (fn: (value: string) => void, delay: number) => {
+  const [debouncedValue, setDebouncedValue] = useState('');
+
+  const onValueChanged = (value: string) => {
+    setDebouncedValue(value);
+  };
+
+  useEffect(
+    () => {
+      const handler = setTimeout(() => {
+        fn(debouncedValue ?? '');
+      }, delay);
+
+      // Cancel the timeout if: value changes, delay change or unmount and passed fn changes
+      return () => {
+        clearTimeout(handler);
+      };
+    },
+    [debouncedValue, delay, fn], // Only re-call effect if value or delay changes
+  );
+
+  return onValueChanged;
+};
