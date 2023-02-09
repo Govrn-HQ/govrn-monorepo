5ab599d69441b8fde1052288139688331320f8fd
jonathanprozzi <jonathanprozzi@gmail.com>
2023-02-09 15:35:28 -0500

    building out each stage

--- apps/protocol-frontend/src/components/SignatureLayout.tsx
+++ apps/protocol-frontend/src/components/SignatureLayout.tsx
@@ -2,28 +2,28 @@ import { useEffect, useState } from 'react';
 import { Link, useSearchParams } from 'react-router-dom';
 import { useAccount } from 'wagmi';
 import { Flex, Button, Text, VisuallyHidden, Heading } from '@chakra-ui/react';
+import { SiDiscord } from 'react-icons/si';
 import ConnectWallet from '../components/ConnectWallet';
 import { useAuth } from '../contexts/AuthContext';
 import { useUser } from '../contexts/UserContext';
 import { GovrnSpinner } from '@govrn/protocol-ui';
 import GovrnTextLogo from './GovrnTextLogo';
 import useDisplayName from '../hooks/useDisplayName';
+import useUserCreate from '../hooks/useUserCreate';
 
 const SignatureLayout = () => {
   const { isConnected, address } = useAccount();
   const { isAuthenticated } = useAuth();
   const [signatureSteps, setSignatureSteps] = useState<number | null>(null);
+
   const [searchParams] = useSearchParams();
   const displayNameFromParams = searchParams.get('displayName');
+  const { mutateAsync: createUser } = useUserCreate();
   console.log('display name', displayNameFromParams);
-
-  // first screen: prompt user to connect wallet
-  // second screen: successful signature -> check if user exists -> if exists, move to third screen or create user with display name first then move to third screen
-  // third screen: discord oauth prompt (can possibly be combined with second screen)
-  // fourth screen: success / error after oauth and next steps
-
   const { userDataByAddress, isUserLoading } = useUser();
   const { displayName } = useDisplayName();
+
+  console.log('userdata', userDataByAddress);
 
   // useEffect(() => {
   //   if (userDataByAddress) {
@@ -33,6 +33,20 @@ const SignatureLayout = () => {
   //     setCreateProfileSteps(1);
   //   }
   // }, [userDataByAddress]);
+
+  const createUserHandler = async () => {
+    if (address && displayName !== undefined) {
+      await createUser({
+        username: displayName || '',
+        address,
+      });
+    }
+  };
+
+  // first screen: prompt user to connect wallet
+  // second screen: successful signature -> check if user exists -> if exists, move to third screen or create user with display name first then move to third screen
+  // third screen: discord oauth prompt (can possibly be combined with second screen)
+  // fourth screen: success / error after oauth and next steps
 
   const NewUserFlow = () => {
     if (isUserLoading) {
@@ -112,13 +126,36 @@ const SignatureLayout = () => {
                 </Text>
                 <ConnectWallet />
               </>
-            ) : (
+            ) : userDataByAddress !== undefined ? (
               <>
-                <Text marginBottom={{ base: 10, lg: 16 }} textAlign="center">
-                  this would be stage 2
+                <Text paddingBottom={8}>
+                  Welcome back,{' '}
+                  <Text as="span" fontWeight="bolder">
+                    {displayName}
+                  </Text>
+                  <Text as="span" fontWeight="normal">
+                    .
+                  </Text>
+                  <Text>
+                    Click the Discord button below to connect your address to
+                    your Discord account.
+                  </Text>
+                  <Text>
+                    You can also use the button below to switch wallets.
+                  </Text>
                 </Text>
-                <ConnectWallet />
+                <Flex
+                  direction={{ base: 'column', lg: 'row' }}
+                  gap={{ base: 2, lg: 4 }}
+                >
+                  <Button variant="tertiary" leftIcon={<SiDiscord />}>
+                    Connect to Discord
+                  </Button>
+                  <ConnectWallet />
+                </Flex>
               </>
+            ) : (
+              <Text>Creating user...</Text>
             )}
           </Flex>
         </Flex>
