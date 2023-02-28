e1f058d2f53f3cee6f2b87cc0081d0cbcd5b46e1
jonathanprozzi <jonathanprozzi@gmail.com>
2023-02-28 15:43:16 -0500

    modified the empty state for the RecentContributions table

--- apps/protocol-frontend/src/components/RecentContributionsTableShell.tsx
+++ apps/protocol-frontend/src/components/RecentContributionsTableShell.tsx
@@ -1,6 +1,5 @@
-import { Box, Stack } from '@chakra-ui/react';
+import { Box, Stack, Text } from '@chakra-ui/react';
 import RecentContributionsTable from './RecentContributionsTable';
-import EmptyContributions from './EmptyContributions';
 import { UIContribution } from '@govrn/ui-types';
 
 interface RecentContributionTableShellProps {
@@ -35,7 +34,36 @@ const RecentContributionsTableShell = ({
           />
         </Stack>
       ) : (
-        <EmptyContributions />
+        <Stack spacing="5">
+          <Box
+            paddingX={{ base: '4', md: '6' }}
+            width="fit-content"
+            paddingY={4}
+          >
+            <Stack
+              direction={{ base: 'column', md: 'row' }}
+              spacing="4"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Text fontSize="lg" fontWeight="medium">
+                No Contributions Yet!{' '}
+                <span
+                  role="img"
+                  aria-labelledby="eyes emoji showcasing that there are not any contributions submitted yet"
+                >
+                  👀
+                </span>
+              </Text>
+            </Stack>
+            <Stack direction="column" spacing="4" paddingTop={8}>
+              <Text fontSize="md" fontWeight="regular" color="gray.900">
+                Get started by adding your first contribution. Click the button
+                below or the "+" in the bottom right corner of the page.
+              </Text>
+            </Stack>
+          </Box>
+        </Stack>
       )}
     </Box>
   );
