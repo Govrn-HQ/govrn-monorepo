2b81015c3b1baec6db1f02c90fed1e731de53454
Jonathan Prozzi <jonathanprozzi@gmail.com>
2023-03-06 10:53:02 -0500

    Add attestation empty states (ENG-946) (#436)
    * Add empty states for my attestation
    * Add empty state for attestations

--- apps/protocol-frontend/src/components/MyAttestationsTable.tsx
+++ apps/protocol-frontend/src/components/MyAttestationsTable.tsx
@@ -1,6 +1,7 @@
 import { useMemo, useState } from 'react';
-
-import { Box, Flex, Stack, Text } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
+import { Box, Flex, Link, Stack, Button, Text } from '@chakra-ui/react';
+import { HiOutlineExternalLink } from 'react-icons/hi';
 import {
   ColumnDef,
   getCoreRowModel,
@@ -15,8 +16,7 @@ import GlobalFilter from './GlobalFilter';
 import { formatDate, toDate } from '../utils/date';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { UIContribution } from '@govrn/ui-types';
-import { GovrnSpinner } from '@govrn/protocol-ui';
-import { Link } from 'react-router-dom';
+import { GovrnCta, GovrnSpinner } from '@govrn/protocol-ui';
 import { useUser } from '../contexts/UserContext';
 import { statusEmojiSelect } from '../utils/statusEmojiSelect';
 import GovrnTable from './GovrnTable';
@@ -50,7 +50,7 @@ const MyAttestationsTable = ({
         }) => {
           return (
             <Flex direction="column" wrap="wrap">
-              <Link to={`/contributions/${row.original.id}`}>
+              <RouterLink to={`/contributions/${row.original.id}`}>
                 <Text
                   whiteSpace="normal"
                   bgGradient="linear-gradient(100deg, #1a202c 0%, #1a202c 100%)"
@@ -63,7 +63,7 @@ const MyAttestationsTable = ({
                 >
                   {getValue()}
                 </Text>
-              </Link>
+              </RouterLink>
             </Flex>
           );
         },
@@ -124,26 +124,113 @@ const MyAttestationsTable = ({
     getFilteredRowModel: getFilteredRowModel(),
   });
 
-  return (
-    <Stack>
-      <GlobalFilter
-        preGlobalFilteredRows={table.getPreFilteredRowModel().rows}
-        globalFilter={globalFilter}
-        setGlobalFilter={setGlobalFilter}
-      />
-      <Box width="100%" maxWidth="100vw" overflowX="auto">
-        <InfiniteScroll
-          dataLength={table.getRowModel().rows.length}
-          next={nextPage}
-          scrollThreshold={0.8}
-          hasMore={hasMoreItems}
-          loader={<GovrnSpinner />}
-        >
-          <GovrnTable controller={table} maxWidth="100vw" overflowX="auto" />
-        </InfiniteScroll>
-      </Box>
-    </Stack>
+  const CopyChildren = () => (
+    <Flex direction="column" alignItems="center" justifyContent="center">
+      <Text>
+        You'll need other collaborators to be part of a DAO! There are no
+        contributions for you to attest to yet.
+      </Text>
+      <Text>
+        Make sure you're part of a DAO and attest to other people's minted
+        contributions.
+      </Text>
+    </Flex>
   );
+
+  const ButtonChildren = () => (
+    <>
+      <Link
+        as={RouterLink}
+        to="/profile"
+        state={{ targetId: 'myDaos' }}
+        _hover={{
+          textDecoration: 'none',
+        }}
+      >
+        <Button
+          variant="primary"
+          size="md"
+          width={{ base: '100%', lg: 'auto' }}
+        >
+          Join a DAO From Your Profile
+        </Button>
+      </Link>
+      <Link
+        href="https://govrn.gitbook.io/govrn-docs/attestations/attestations"
+        isExternal
+        textDecoration="none"
+        _hover={{
+          textDecoration: 'none',
+        }}
+      >
+        <Button
+          variant="secondary"
+          size="md"
+          leftIcon={<HiOutlineExternalLink />}
+        >
+          Learn More About Attestations
+        </Button>
+      </Link>
+    </>
+  );
+
+  let component = (
+    <GovrnCta
+      heading={`You haven't attested yet`}
+      emoji="🙏"
+      copy={<CopyChildren />}
+      children={<ButtonChildren />}
+    />
+  );
+
+  if (data.length) {
+    component = (
+      <>
+        <Box paddingX={{ base: '4', md: '6' }} paddingTop="5" paddingBottom="3">
+          <Stack
+            direction={{ base: 'column', md: 'row' }}
+            justify="space-between"
+            alignItems="bottom"
+          >
+            <Stack direction="column" gap="2">
+              <Text fontSize="lg" fontWeight="medium">
+                My Contributions
+              </Text>
+              <Text fontSize="md" fontWeight="normal">
+                These are contributions that you have already attested to.
+              </Text>
+            </Stack>
+          </Stack>
+        </Box>
+
+        <Stack marginTop="5">
+          <Flex alignItems="center">
+            <GlobalFilter
+              preGlobalFilteredRows={table.getPreFilteredRowModel().rows}
+              globalFilter={globalFilter}
+              setGlobalFilter={setGlobalFilter}
+            />
+          </Flex>
+          <Box width="100%" maxWidth="100vw" overflowX="auto">
+            <InfiniteScroll
+              dataLength={table.getRowModel().rows.length}
+              next={nextPage}
+              scrollThreshold={0.8}
+              hasMore={hasMoreItems}
+              loader={<GovrnSpinner />}
+            >
+              <GovrnTable
+                controller={table}
+                maxWidth="100vw"
+                overflowX="auto"
+              />
+            </InfiniteScroll>
+          </Box>
+        </Stack>
+      </>
+    );
+  }
+  return component;
 };
 
 export default MyAttestationsTable;
