5937adfed76000793a10c4769216990b05f9d327
Flip <13227294+Flip-Liquid@users.noreply.github.com>
2023-03-23 22:56:12 -0600

    manual resolve verificaiton setting; add db functions

--- apps/jobs/contribution-verification/src/db.ts
+++ apps/jobs/contribution-verification/src/db.ts
@@ -11,13 +11,40 @@ export const govrn = new GovrnProtocol(PROTOCOL_URL, null, {
 export async function getDaosWithVerificationSettings() {
   return await govrn.guild.list({
     where: {
-      verificationSettings: {
-        some: {
-          id: {
-            not: null,
-          },
-        },
+      verification_setting_id: {
+        not: null,
       },
     },
   });
+}
+
+export async function getDaoVerificationThreshold(
+  verificationSettingId: number,
+) {
+  const setting = await govrn.guildVerificationSetting.get({
+    id: verificationSettingId,
+  });
+  return setting.num_of_attestations;
+}
+
+export async function getUnverifiedContributions(daoId: number) {
+  const result = await govrn.guildContribution.list({
+    where: {
+      guild_id: {
+        equals: daoId,
+      },
+      verificationStatus: {
+        is: {
+          name: { equals: 'unverified' },
+        },
+      },
+      /**
+       * TODO: is the verified field redundant?
+       * verified: {
+       *  equals: false,
+       * },
+       */
+    },
+  });
+  return result.result;
 }
--- apps/jobs/contribution-verification/src/main.ts
+++ apps/jobs/contribution-verification/src/main.ts
@@ -1,4 +1,11 @@
-import { getDaosWithVerificationSettings } from './db';
+import { batch } from '@govrn/protocol-client';
+import {
+  getDaosWithVerificationSettings,
+  getDaoVerificationThreshold,
+  getUnverifiedContributions,
+} from './db';
+
+const BATCH_SIZE = 100;
 
 const main = async () => {
   console.log('starting contribution verification');
@@ -9,4 +16,36 @@ const main = async () => {
 
   // Retrieve all daos which have defined verifications settings
   const daos = await getDaosWithVerificationSettings();
+
+  for (const dao of daos) {
+    console.log('dao', dao);
+    const id = dao.id;
+    const name = dao.name;
+
+    const settingThreshold = await getDaoVerificationThreshold(
+      dao.verification_setting_id,
+    );
+
+    const unverifiedContributions = await getUnverifiedContributions(id);
+
+    // process the guilds unverified contributions
+    await batch(
+      unverifiedContributions,
+      async guildContribution => {
+        console.log('contribution', guildContribution);
+        const contributionId = guildContribution.id;
+        const attestationCount =
+          guildContribution.contribution.attestations.length;
+        const verified = guildContribution.verified;
+
+        if (!verified && attestationCount >= settingThreshold) {
+          console.log(
+            `verifying dao: ${name} contribution: ${contributionId} attestationCount: ${attestationCount} threshold: ${settingThreshold}`,
+          );
+          await upsertVerifiedGuildContribution(contributionId);
+        }
+      },
+      BATCH_SIZE,
+    );
+  }
 };
