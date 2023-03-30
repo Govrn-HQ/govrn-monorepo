49c93c598911fc16a26d1dcee51ab5751b0b0432
Flip-Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2023-03-30 06:40:43 -0700

    Add  contribution verification job (#465)
    Co-authored-by: Alexander Keating <keating.dev@protonmail.com>

--- apps/protocol-api/src/prisma/generated/type-graphql/models/GuildContribution.ts
+++ apps/protocol-api/src/prisma/generated/type-graphql/models/GuildContribution.ts
@@ -44,11 +44,6 @@ export class GuildContribution {
   })
   verification_status_id?: number | null;
 
-  @TypeGraphQL.Field(_type => Boolean, {
-    nullable: false
-  })
-  verified!: boolean;
-
   @TypeGraphQL.Field(_type => TypeGraphQL.Int, {
     nullable: true
   })
--- apps/protocol-api/src/prisma/generated/type-graphql/resolvers/inputs/GuildContributionUpdateInput.ts
+++ apps/protocol-api/src/prisma/generated/type-graphql/resolvers/inputs/GuildContributionUpdateInput.ts
@@ -2,7 +2,6 @@ import * as TypeGraphQL from "type-graphql";
 import * as GraphQLScalars from "graphql-scalars";
 import { Prisma } from "@prisma/client";
 import { DecimalJSScalar } from "../../scalars";
-import { BoolFieldUpdateOperationsInput } from "../inputs/BoolFieldUpdateOperationsInput";
 import { ContributionUpdateOneRequiredWithoutGuildsNestedInput } from "../inputs/ContributionUpdateOneRequiredWithoutGuildsNestedInput";
 import { DateTimeFieldUpdateOperationsInput } from "../inputs/DateTimeFieldUpdateOperationsInput";
 import { GuildContributionVerificationStatusUpdateOneWithoutGuild_contributionsNestedInput } from "../inputs/GuildContributionVerificationStatusUpdateOneWithoutGuild_contributionsNestedInput";
@@ -32,11 +31,6 @@ export class GuildContributionUpdateInput {
     nullable: true
   })
   contribution?: ContributionUpdateOneRequiredWithoutGuildsNestedInput | undefined;
-
-  @TypeGraphQL.Field(_type => BoolFieldUpdateOperationsInput, {
-    nullable: true
-  })
-  verified?: BoolFieldUpdateOperationsInput | undefined;
 
   @TypeGraphQL.Field(_type => NullableIntFieldUpdateOperationsInput, {
     nullable: true
