e31acdf226d2ae606505dcce71763511e7f88a73
Alexander Keating <keating.dev@protonmail.com>
2023-04-12 17:11:20 -0400

    Revert "Second fix for dao attribute (#490)"
    This reverts commit df12b23f80390073239036d41f385f2b74b9e1b9.

--- apps/protocol-frontend/src/components/ReportForm.tsx
+++ apps/protocol-frontend/src/components/ReportForm.tsx
@@ -189,13 +189,9 @@ const ReportForm = ({ onFinish }: { onFinish: () => void }) => {
 
   useEffect(() => {
     const matchedDao = daoListOptions.find(dao => dao.value === daoIdParam);
-
-    setValue('daoId', matchedDao?.value ?? null); // allows user to submit contribution with a preset daoId query param without needing to touch the field
-  }, [setValue, daoListOptions, daoIdParam]);
-
-  useEffect(() => {
     setValue('engagementDate', engagementDateValue);
-  }, [engagementDateValue, setValue]);
+    setValue('daoId', matchedDao?.value ?? null); // allows user to submit contribution with a preset daoId query param without needing to touch the field
+  }, [engagementDateValue, setValue, daoListOptions, daoIdParam]);
 
   const {
     data: guildActivityTypeListData,
