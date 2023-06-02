d6b9870827c0bb8fa4c49c190d12eed4ad772f96
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2023-06-02 13:21:46 -0400

    add tweet conversion from snscrape format to internal

--- apps/jobs/twitter-contribution/twitter_contribution/data.py
+++ apps/jobs/twitter-contribution/twitter_contribution/data.py
@@ -1,4 +1,5 @@
 import datetime
+import snscrape.modules.twitter as snscrape
 
 
 class Tweet:
@@ -7,3 +8,12 @@ class Tweet:
     profile_handle: str
     date: str
     url: str
+
+    def from_scraped_tweet(scraped_tweet: snscrape.Tweet):
+        tweet = Tweet()
+        tweet.url = scraped_tweet.url
+        tweet.id = scraped_tweet.id
+        tweet.profile_handle = scraped_tweet.user.username
+        tweet.content = scraped_tweet.rawContent
+        tweet.date = scraped_tweet.date.isoformat()
+        return tweet
