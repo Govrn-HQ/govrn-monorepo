d55b325c2289d22331d6d42eca02b0e6eebc52fc
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2023-06-06 22:57:35 -0400

    format json logs

--- apps/jobs/twitter-contribution/twitter_contribution/utils.py
+++ apps/jobs/twitter-contribution/twitter_contribution/utils.py
@@ -12,13 +12,16 @@ def now() -> datetime:
     return datetime.now(timezone.utc)
 
 
+handler = logging.StreamHandler()
 formatter = json_log_formatter.JSONFormatter(
     "%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s"
 )
+handler.setFormatter(formatter)
 
 
 def get_logger(name: str):
     logger = logging.getLogger(name)
+    logger.addHandler(handler)
     logger.setLevel(logging.INFO)
 
     return logger
